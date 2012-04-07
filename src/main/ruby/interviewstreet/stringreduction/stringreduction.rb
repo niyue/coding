59e18f336e02af75b2b29db56b280c7802abfde7
Yue Ni <niyue.com@gmail.com>
2012-04-07 17:04:51 +0800

    Implement with an O(1) method to compute the reduction count but I don't understand how to deduce this formula.

--- src/main/ruby/interviewstreet/stringreduction/stringreduction.rb
+++ src/main/ruby/interviewstreet/stringreduction/stringreduction.rb
@@ -1,45 +1,34 @@
-def reduction_count(string, count_table)
+def reduction_count(string)
   if(string.length == 1)
     return 1
-  elsif(string.length == 2)
-    return is_reducible(string, 0) ? 1 : 2
   end
-  count = string.length
-  for i in 0..(string.length-2)
-    if is_reducible(string, i)
-      reduced_string = reduce(string, i)
-      #if count_table[reduced_string].nil?
-        reduced_count = reduction_count(reduced_string, count_table)
-      #else
-      #  reduced_count = count_table[reduced_string]
-      #end
-      if reduced_count < count
-        count = reduced_count
-      end
+  
+  a_count = 0
+  b_count = 0
+  c_count = 0
+  
+  for c in string
+    if(c.eql?('a'))
+      a_count += 1
+    elsif(c.eql?('b'))
+      b_count += 1
+    else
+      c_count += 1
     end
-  end  
-  #count_table[string] = count
-  return count
-end
-
-def reduce(string, index)
-  one = string[index].bytes.next - ?a.ord
-  two = string[index+1].bytes.next - ?a.ord
-  sum = one + two
-  new_char = 'a'
-  if sum == 1 # a,b
-    new_char = 'c'
-  elsif sum == 2
-    new_char = 'b'
   end
-  reduced_string = Array.new(string)  
-  reduced_string.delete_at(index)
-  reduced_string[index] = new_char
-  return reduced_string
-end
-
-def is_reducible(string, index)
-  return string[index] != string[index+1]
+  
+  if(a_count == 0 && b_count == 0 || b_count == 0 && c_count == 0 || c_count == 0 && a_count ==0)
+    return string.length
+  end
+  
+  a_odd = a_count % 2 != 0
+  b_odd = b_count % 2 != 0
+  c_odd = c_count % 2 != 0
+  
+  if(a_odd && b_odd && c_odd || !a_odd && !b_odd && !c_odd)
+    return 2
+  end
+  return 1
 end
 
 t = gets.chomp.to_i
@@ -50,5 +39,5 @@ end
 
 count_table = {}
 for string in strings
-  puts reduction_count(string, count_table)
+  puts reduction_count(string)
 end
