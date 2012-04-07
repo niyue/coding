5703382f8288ef76640293a0e36584d53969a906
Yue Ni <niyue.com@gmail.com>
2012-04-07 18:03:39 +0800

    Split the digits into sqrt(N) partitions for searching. Both search is O(sqrt(N)) and update operation is O(1). Got 8/11 TLE.

--- src/main/java/com/niyue/coding/interviewstreet/changingbits/Solution.java
+++ src/main/java/com/niyue/coding/interviewstreet/changingbits/Solution.java
@@ -1,54 +1,80 @@
 package com.niyue.coding.interviewstreet.changingbits;
 
-import java.util.NavigableSet;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
 import java.util.Scanner;
-import java.util.TreeSet;
+import java.util.Set;
+
 
 public class Solution {
     private int N, Q;
     private int[] A, B;
     private Scanner scanner = new Scanner(System.in);
-    private NavigableSet<Integer> equalBits;
-
+    private List<Set<Integer>> equalBits;
+    private int noOfPartitions;
+    
     public static void main(String[] args) throws java.lang.Exception {
         Solution sl = new Solution();
         sl.solve();
     }
-
+ 
     public void solve() {
         getInput();
-        for (int i = 0; i < Q; i++) {
+        for(int i=0;i<Q;i++) {
             String op = scanner.next();
             char opCode = op.charAt(op.length() - 1);
             int bit = scanner.nextInt();
-            if (opCode == 'c') {
+            if(opCode == 'c') {
                 System.out.print(calc(A, B, bit));
-            } else {
+            }
+            else {
                 int[] number = opCode == 'a' ? A : B;
                 int bitSet = scanner.nextInt();
-                if (number[bit] != bitSet) {
+                if(number[bit] != bitSet) {
                     number[bit] = bitSet;
-                    if (A[bit] == B[bit]) {
-                        equalBits.add(bit);
-                    } else {
-                        equalBits.remove(bit);
+                    int partition = bit / noOfPartitions;
+                    if(A[bit] == B[bit]) {
+                        equalBits.get(partition).add(bit);
+                    }
+                    else {
+                        equalBits.get(partition).remove(bit);
                     }
                 }
             }
         }
     }
-
+    
     private int calc(int[] a, int[] b, int bit) {
         int higherSum = bit == N ? 0 : a[bit] + b[bit];
-        Integer equalBit = equalBits.lower(bit);
-        if (equalBit != null) {
-            if (a[equalBit] == 1) {
+        Integer equalBit = searchLowerEqualBits(bit);
+        if(equalBit != null) {
+            if(a[equalBit] == 1) {
                 higherSum++;
             }
         }
         return higherSum & 1;
     }
-
+    
+    private Integer searchLowerEqualBits(int bit) {
+        int partition = bit / noOfPartitions;
+        Integer maxLowerEqualBit = null;
+        for(int i=partition;i>=0;i--) {
+            Set<Integer> equalBitsInPartition = equalBits.get(i);
+            if(!equalBitsInPartition.isEmpty()) {
+                for(Integer equalBit : equalBitsInPartition) {
+                    if(equalBit < bit) {
+                        if(maxLowerEqualBit == null || equalBit > maxLowerEqualBit) {
+                            maxLowerEqualBit = equalBit;
+                        }
+                    }
+                }
+                break;
+            }
+        }
+        return maxLowerEqualBit;
+    }
+    
     private void getInput() {
         N = scanner.nextInt();
         Q = scanner.nextInt();
@@ -56,22 +82,28 @@ public class Solution {
         String b = scanner.next();
         A = asBinary(a);
         B = asBinary(b);
+        noOfPartitions = ((int)Math.sqrt(N)) + 1;
         equalBits = findEqualBits(A, B);
     }
-
+    
     private int[] asBinary(String number) {
         int[] binary = new int[N];
-        for (int i = 0; i < number.length(); i++) {
-            binary[N - 1 - i] = number.charAt(i) - '0';
+        for(int i=0;i<number.length();i++) {
+            binary[N-1-i] = number.charAt(i) - '0';
         }
         return binary;
     }
-
-    private NavigableSet<Integer> findEqualBits(int[] a, int[] b) {
-        NavigableSet<Integer> bits = new TreeSet<Integer>();
-        for (int i = 0; i < N; i++) {
-            if (a[i] == b[i]) {
-                bits.add(i);
+    
+    private List<Set<Integer>> findEqualBits(int[] a, int[] b) {
+        List<Set<Integer>> bits = new ArrayList<Set<Integer>>(noOfPartitions);
+        int partitionSize = noOfPartitions;
+        for(int i=0;i<noOfPartitions;i++) {
+            bits.add(new HashSet<Integer>(partitionSize));
+        }
+        for(int i=0;i<N;i++) {
+            if(a[i] == b[i]) {
+                int partition = i / partitionSize;
+                bits.get(partition).add(i);
             }
         }
         return bits;
