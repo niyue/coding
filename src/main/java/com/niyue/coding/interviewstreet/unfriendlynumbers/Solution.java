03dce16676b34b50b76a18559a94734142c5f3bb
Yue Ni <niyue.com@gmail.com>
2012-05-14 08:21:39 +0800

    Avoid computation of factors of K, and compute factors for only necessary unfriendly numbers to minimize unused computation. Not verify yet, the website seems problematic at the moment.

--- src/main/java/com/niyue/coding/interviewstreet/unfriendlynumbers/Solution.java
+++ src/main/java/com/niyue/coding/interviewstreet/unfriendlynumbers/Solution.java
@@ -15,7 +15,6 @@ class Solution {
     private int N;
     private long K;
     private Map<Long, Set<Long>> childFactorsMap = new HashMap<Long, Set<Long>>();
-    private Set<Long> computedNumbers = new HashSet<Long>();
 
     public static void main(String[] args) throws java.lang.Exception {
         Solution sl = new Solution();
@@ -27,21 +26,34 @@ class Solution {
         
         Map<Long, Integer> primeFactorsCount = factorize(K);
         
-        Set<Long> factors = factors(primeFactorsCount);
-        
-        Set<Long> combinedFactors = new HashSet<Long>();
-        
+        Set<Long> unfriendlyEquivalentNumbers = new HashSet<Long>();
+        Map<Long, Map<Long, Integer>> unfriendlyPrimeFactorsCount = new HashMap<Long, Map<Long, Integer>>();
         for(long unfriendlyNumber : unfriendlyNumbers) {
             Map<Long, Integer> numberPrimeFactorsCount = primeFactorsCount(unfriendlyNumber, primeFactorsCount);
             long equivalentNumber = equivalentNumber(numberPrimeFactorsCount);
-            if(!computedNumbers.contains(equivalentNumber)) {
-                Set<Long> subFactors = childFactorsMap.get(equivalentNumber);
-                combinedFactors.addAll(subFactors);
-                computedNumbers.add(equivalentNumber);
-            }
+            unfriendlyEquivalentNumbers.add(equivalentNumber);
+            unfriendlyPrimeFactorsCount.put(equivalentNumber, numberPrimeFactorsCount);
         }
         
-        System.out.println(factors.size() - combinedFactors.size());
+        Set<Long> combinedFactors = new HashSet<Long>();
+        for(long number : unfriendlyEquivalentNumbers) {
+            Map<Long, Integer> numberPrimeFactorsCount = unfriendlyPrimeFactorsCount.get(number);
+            Set<Long> subFactors = factors(number, numberPrimeFactorsCount, new HashSet<Long>());
+            combinedFactors.addAll(subFactors);
+        }
+        
+        int totalCombinations = factorsCombinations(primeFactorsCount);
+        
+        System.out.println(totalCombinations - combinedFactors.size());
+    }
+    
+    private int factorsCombinations(Map<Long, Integer> primeFactorsCount) {
+        int combinations = 1;
+        for(Entry<Long, Integer> primeFactorCount : primeFactorsCount.entrySet()) {
+            int count = primeFactorCount.getValue();
+            combinations *= count + 1;
+        }
+        return combinations;
     }
     
     private Map<Long, Integer> factorize(long k) {
@@ -142,22 +154,30 @@ class Solution {
         return product;
     }
     
-    private Set<Long> factors(Map<Long, Integer> primeFactorsCount) {
+    private Set<Long> factors(long equivalentNumber, Map<Long, Integer> primeFactorsCount, Set<Long> computedPrimes) {
         Set<Long> factors = new HashSet<Long>();
-        long equivalentNumber = equivalentNumber(primeFactorsCount);
+        
         if(childFactorsMap.containsKey(equivalentNumber)) {
             factors = childFactorsMap.get(equivalentNumber);
         }
         else {
-            if(!primeFactorsCount.isEmpty()) {
-                for(Entry<Long, Integer> primeFactorCount : primeFactorsCount.entrySet()) {
-                    long prime = primeFactorCount.getKey();
-                    Map<Long, Integer> subPrimeFactorsCount = decreaseCount(primeFactorsCount, prime);
-                    Set<Long> subFactors = factors(subPrimeFactorsCount);
-                    factors.addAll(subFactors);
-                    for(Long factor : subFactors) {
-                        long largeFactor = factor * prime; 
-                        factors.add(largeFactor);
+            if(primeFactorsCount.entrySet().size() > computedPrimes.size()) {
+                for(Entry<Long, Integer> primeCount : primeFactorsCount.entrySet()) {
+                    long prime = primeCount.getKey();
+                    int count = primeCount.getValue();
+                    if(count > 0) {
+                        primeFactorsCount.put(prime, count - 1);
+                        if(count == 1) {
+                            computedPrimes.add(prime);
+                        }
+                        Set<Long> subFactors = factors(equivalentNumber / prime, primeFactorsCount, computedPrimes);
+                        factors.addAll(subFactors);
+                        for(Long factor : subFactors) {
+                            long largeFactor = factor * prime; 
+                            factors.add(largeFactor);
+                        }
+                        primeFactorsCount.put(prime, count);
+                        computedPrimes.remove(prime);
                     }
                 }
             }
@@ -167,18 +187,6 @@ class Solution {
             childFactorsMap.put(equivalentNumber, factors);
         }
         return factors;
-    }
-    
-    private Map<Long, Integer> decreaseCount(Map<Long, Integer> primeFactorsCount, long prime) {
-        int count = primeFactorsCount.get(prime);
-        Map<Long, Integer> subPrimeFactorsCount = new LinkedHashMap<Long, Integer>(primeFactorsCount);
-        if(count > 1) {
-            subPrimeFactorsCount.put(prime, count - 1);
-        }
-        else {
-            subPrimeFactorsCount.remove(prime);
-        }
-        return subPrimeFactorsCount;
     }
     
     private List<Long> getInput() {
