6b0762c9c2839f4ea2bcba50cc195c499a8b704c
Ni Yue <niyue.com@gmail.com>
2012-11-18 12:10:42 +0800

    Add some test cases for grid walking and meeting point.

---
+++ src/test/java/com/niyue/coding/interviewstreet/gridwalking/SolutionTest.java
@@ -0,0 +1,28 @@
+package com.niyue.coding.interviewstreet.gridwalking;
+
+import java.util.Collection;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
+
+import com.niyue.coding.BaseTest;
+import com.niyue.coding.DataLoader;
+
+@RunWith(Parameterized.class)
+public class SolutionTest extends BaseTest {
+    public SolutionTest(String input, String output) {
+        super(input, output);
+    }
+    
+    @Parameters
+    public static Collection<String[]> data() {
+        return DataLoader.load(SolutionTest.class);
+    }
+
+    @Override
+    protected void solve() throws Exception {
+        Solution solution = new Solution();
+        solution.solve();
+    }
+}
