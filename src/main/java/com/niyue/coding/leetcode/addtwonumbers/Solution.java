a237527a3dd47b3563dbde3835f415a2fba298ba
Ni Yue <niyue.com@gmail.com>
2013-01-24 23:46:31 +0800

    Solve the sum two linked list numbers problem in leetcode.

---
+++ src/main/java/com/niyue/coding/leetcode/addtwonumbers/Solution.java
@@ -0,0 +1,52 @@
+package com.niyue.coding.leetcode.addtwonumbers;
+
+// http://leetcode.com/onlinejudge#question_2
+
+public class Solution {
+	// Definition for singly-linked list.
+	public static class ListNode {
+		int val;
+		ListNode next = null;
+		ListNode(int x) {
+			val = x;
+		}
+	}
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        int carry = 0;
+        ListNode s1 = l1;
+        ListNode s2 = l2;
+        ListNode result = null;
+        ListNode current = null;
+        // sum both list
+        while(s1 != null && s2 != null) {
+            int sum = s1.val + s2.val + carry;
+            carry = sum / 10;
+            sum = sum % 10;
+            if(current == null) {
+                current = new ListNode(sum);
+                result = current;
+            } else {
+                current.next = new ListNode(sum);
+                current = current.next;
+            }
+            s1 = s1.next;
+            s2 = s2.next;
+        }
+        // copy the remaining numbers in longer one
+        if(s1 != null || s2 != null) {
+            ListNode remaining = s1 != null ? s1 : s2;
+            while(remaining != null) {
+                int sum = remaining.val + carry;
+                carry = sum / 10;
+                sum = sum % 10;    
+                current.next = new ListNode(sum);
+                current = current.next;
+                remaining = remaining.next;
+            }
+        }
+        if(carry == 1) {
+            current.next = new ListNode(1);    
+        }
+        return result;
+    }
+}
