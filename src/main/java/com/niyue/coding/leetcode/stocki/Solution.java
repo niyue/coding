1b0d8038a929688dee6a813cae44b3008a2e5955
Yue Ni <niyue.com@gmail.com>
2013-02-15 13:48:32 +0800

    Solve the stock buying problem with a more simpler solution, still an O(n) solution but it is much simpler to implement.

--- src/main/java/com/niyue/coding/leetcode/stocki/Solution.java
+++ src/main/java/com/niyue/coding/leetcode/stocki/Solution.java
@@ -5,37 +5,20 @@ package com.niyue.coding.leetcode.stocki;
 // If you were only permitted to complete at most one transaction (ie, buy one and sell one share of the stock), design an algorithm to find the maximum profit.
 public class Solution {
     public int maxProfit(int[] prices) {
-        int[] minPrices = leftMinPrices(prices);
-        int[] maxPrices = rightMaxPrices(prices);
         int maxProfit = 0;
-        for(int i = 0;i < prices.length - 1;i++) {
-            int profit = maxPrices[i + 1] - minPrices[i];
-            if(profit > maxProfit) {
-                maxProfit = profit;
-            }
+        if(prices.length >= 2) {
+            int min = prices[0];
+            for(int i = 1;i < prices.length;i++) {
+                int profit = prices[i] - min;
+                if(profit > maxProfit) {
+                    maxProfit = profit;
+                }
+                if(prices[i] < min) {
+                    min = prices[i];
+                }
+            }    
         }
+        
         return maxProfit;
     }
-    
-    private int[] leftMinPrices(int[] prices) {
-        int[] minPrices = new int[prices.length];
-        for(int i = 0;i < prices.length;i++) {
-            int minPrice = i == 0 ? prices[0] : Math.min(minPrices[i - 1], prices[i]);
-            minPrices[i] = minPrice;
-        }
-        return minPrices;
-    }
-    
-    private int[] rightMaxPrices(int[] prices) {
-        int[] maxPrices = new int[prices.length];
-        int lastPriceIndex = prices.length - 1;
-        for(int i = lastPriceIndex;i >= 0;i--) {
-            int maxPrice = i == lastPriceIndex 
-                ? prices[lastPriceIndex] 
-                : Math.max(maxPrices[i + 1], prices[i]);
-            maxPrices[i] = maxPrice;
-        }
-        return maxPrices;
-    }
-    
 }
