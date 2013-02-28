8fc44479195fcbed70b7e698a29f008ee2f686f5
Yue Ni <niyue.com@gmail.com>
2013-02-28 18:13:56 +0800

    Solve the 'partition list' problem in leetcode. Partition list to two lists according to their values and combine these two lists.

---
+++ src/main/java/com/niyue/coding/leetcode/partitionlist/Solution.java
@@ -0,0 +1,41 @@
+package com.niyue.coding.leetcode.partitionlist;
+
+// http://leetcode.com/onlinejudge#question_86
+// Partition the linked list to two lists (one for small values, one for big values), just like the partition in quick sort, and combine two lists finally
+public class Solution {
+    public ListNode partition(ListNode head, int x) {
+        ListNode small = new ListNode(0);
+        ListNode big = new ListNode(0);
+        ListNode current = head;
+        ListNode smallCursor = small;
+        ListNode bigCursor = big;
+        while(current != null) {
+            if(current.val < x) {
+                smallCursor.next = current;
+                smallCursor = smallCursor.next;
+            } else {
+                bigCursor.next = current;
+                bigCursor = bigCursor.next;
+            }
+            current = current.next;
+        }
+        small = small.next;
+        big = big.next;
+        bigCursor.next = null;
+        smallCursor.next = null;
+        if(small != null) {
+        	smallCursor.next = big;    
+        } else if(big != null) {
+            small = big;
+        }
+        return small;
+    }
+    
+    public static final class ListNode {
+    	public int val;
+    	public ListNode next;
+    	public ListNode(int val) {
+    		this.val = val;
+    	}
+    }
+}
---
+++ src/test/java/com/niyue/coding/leetcode/partitionlist/PartitionListTest.java
@@ -0,0 +1,35 @@
+package com.niyue.coding.leetcode.partitionlist;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
+
+import org.junit.Test;
+
+import com.niyue.coding.leetcode.partitionlist.Solution.ListNode;
+
+public class PartitionListTest {
+
+	@Test
+	// fail to consider there may be a next reference doesn't set to null after iterating all nodes
+	public void testTwoElements() {
+		ListNode head = new ListNode(2);
+		head.next = new ListNode(1);
+		Solution sl = new Solution();
+		ListNode list = sl.partition(head, 2);
+		assertEquals(1, list.val);
+		assertEquals(2, list.next.val);
+		assertNull(list.next.next);
+	}
+	
+	@Test
+	// use the incorrect cursor (should use the tail cursor instead of the head cursor) when link the small value list to the big value list
+	public void testTwoSameElements() {
+		ListNode head = new ListNode(1);
+		head.next = new ListNode(1);
+		Solution sl = new Solution();
+		ListNode list = sl.partition(head, 2);
+		assertEquals(1, list.val);
+		assertEquals(1, list.next.val);
+		assertNull(list.next.next);
+	}
+}
