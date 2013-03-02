557f95f8a73601c0269ad7b6218022d632cea594
Yue Ni <niyue.com@gmail.com>
2013-03-02 20:30:26 +0800

    Convert sorted linked list to balanced binary search tree in O(n).

---
+++ src/main/java/com/niyue/coding/leetcode/sortedlisttobst/Solution.java
@@ -0,0 +1,56 @@
+package com.niyue.coding.leetcode.sortedlisttobst;
+
+// http://leetcode.com/onlinejudge#question_109
+// O(n) solution to convert sorted linked list to BST
+public class Solution {
+    public TreeNode sortedListToBST(ListNode head) {
+        int length = getLength(head);
+        
+        return (TreeNode) convert(head, 0, length - 1)[0];
+    }
+    
+    private int getLength(ListNode head) {
+        int length = 0;
+        for(ListNode cursor = head; cursor != null; cursor = cursor.next) {
+            length++;
+        }
+        return length;
+    }
+    
+    private Object[] convert(ListNode node, int start, int end) {
+        TreeNode root = null;
+        if(start <= end) {
+            int mid = start + (end - start) / 2;
+            Object[] treeNodeAndListNode = convert(node, start, mid - 1);
+            TreeNode left = (TreeNode) treeNodeAndListNode[0];
+            node = (ListNode) treeNodeAndListNode[1];
+            TreeNode parent = new TreeNode(node.val);
+            node = node.next;
+            treeNodeAndListNode = convert(node, mid + 1, end);
+            TreeNode right = (TreeNode) treeNodeAndListNode[0];
+            node = (ListNode) treeNodeAndListNode[1];
+            parent.left = left;
+            parent.right = right;    
+            root = parent;
+        }
+        
+        return new Object[] {root, node};
+    }
+    
+    public static final class ListNode {
+    	int val;
+    	ListNode next;
+    	ListNode(int x) {
+    		this.val = x;
+    	}
+    }
+    
+    public static final class TreeNode {
+    	int val;
+    	TreeNode left;
+    	TreeNode right;
+    	TreeNode(int val) {
+    		this.val = val;
+    	}
+    }
+}
---
+++ src/test/java/com/niyue/coding/leetcode/sortedlisttobst/SortedListToBstTest.java
@@ -0,0 +1,59 @@
+package com.niyue.coding.leetcode.sortedlisttobst;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
+
+import org.junit.Test;
+
+import com.niyue.coding.leetcode.sortedlisttobst.Solution.ListNode;
+import com.niyue.coding.leetcode.sortedlisttobst.Solution.TreeNode;
+
+public class SortedListToBstTest {
+
+	@Test
+	public void testLinkedListToBstSingleElement() {
+		ListNode list = newLinkedList(1);
+		Solution sl = new Solution();
+		TreeNode root = sl.sortedListToBST(list);
+		assertEquals(1, root.val);
+		assertNull(root.left);
+		assertNull(root.right);
+	}
+	
+	@Test
+	public void testLinkedListToBstTwoElements() {
+		ListNode list = newLinkedList(1, 2);
+		Solution sl = new Solution();
+		TreeNode root = sl.sortedListToBST(list);
+		assertEquals(1, root.val);
+		assertEquals(2, root.right.val);
+		assertNull(root.left);
+	}
+	
+	@Test
+	// stupid mistake, Java is pass-by-value, "node = node.next" doesn't advance the cursor at all
+	public void testLinkedListToBst123() {
+		ListNode list = newLinkedList(1, 2, 3);
+		Solution sl = new Solution();
+		TreeNode root = sl.sortedListToBST(list);
+		assertEquals(2, root.val);
+		assertEquals(1, root.left.val);
+		assertEquals(3, root.right.val);
+	}
+	
+	private ListNode newLinkedList(int... values) {
+		ListNode head = null;
+		ListNode prev = null;
+		for(int v : values) {
+			ListNode current = new ListNode(v);
+			if(head == null) {
+				head = current;
+			}
+			if(prev != null) {
+				prev.next = current;
+			}
+			prev = current;
+		}
+		return head;
+	}
+}
