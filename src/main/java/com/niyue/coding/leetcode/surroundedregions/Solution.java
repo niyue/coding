7ab667e354ba48f8c6fe6b93d5eea59cc9274033
Yue Ni <niyue.com@gmail.com>
2013-03-03 17:35:13 +0800

    Implemented a O(n^2) time and constant space way to solve the 'surrounded regions' problem. however, it turns out this method still requires deep recursion and caused stack overflow in large data set given.

--- src/main/java/com/niyue/coding/leetcode/surroundedregions/Solution.java
+++ src/main/java/com/niyue/coding/leetcode/surroundedregions/Solution.java
@@ -1,14 +1,10 @@
 package com.niyue.coding.leetcode.surroundedregions;
 
-import java.util.Collection;
-import java.util.Collections;
-import java.util.LinkedList;
 
 // http://leetcode.com/onlinejudge#question_130
-// O(n^2) time and O(n^2) space complexity solution. flood fill to detect if a region is surrounded
-// this solution needs to traverse the board only once
+// O(n^2) time and O(1) space complexity solution. 
+// flood fill from cells in border
 public class Solution {
-	private boolean[][] visited;
 	private char[][] board;
 	private int height;
 	private int width;
@@ -16,91 +12,76 @@ public class Solution {
     public void solve(char[][] inputBoard) {
     	board = inputBoard;
     	if(inputBoard.length > 1 && inputBoard[0].length > 1) {
-    		visited = new boolean[board.length][board[0].length];
-	    	width = board.length;
-	    	height = board[0].length;
+    		width = board.length;
+    		height = board[0].length;
 	    	
+    		for(int y = 0; y < height; y++) {
+    			markUnsurrounded(0, y);
+    		}
+    		
+    		for(int y = 0; y < height; y++) {
+    			markUnsurrounded(width - 1, y);
+    		}
+    		
+    		for(int x = 0; x < width; x++) {
+    			markUnsurrounded(x, 0);
+    		}
+    		
+    		for(int x = 0; x < width; x++) {
+    			markUnsurrounded(x, height - 1);
+    		}
+    		
 	    	for(int x = 0; x < board.length; x++) {
 	    		for(int y = 0; y < board[x].length; y++) {
-	    			if(isO(x, y) && isUnvisited(x, y)) {
-	    				Collection<Cell> surroundedCells = surroundedCells(x, y);
-	    				for(Cell cell : surroundedCells) {
-	    					flip(cell);
-	    				}
+	    			if(isO(x, y)) {
+	    				flip(x, y, 'X');
+	    			} else if(isU(x, y)) {
+	    				flip(x, y, 'O');
 	    			}
 	    		}
 	    	}
     	}
     }
-
-    private Collection<Cell> surroundedCells(int x, int y) {
-    	Collection<Cell> surroundedCells = new LinkedList<Cell>();
-    	surroundedCells.add(new Cell(x, y));
-    	visit(x, y);
-    	if(isBorder(x, y)) {
-    		surroundedCells = Collections.emptyList();
+    
+    private void markUnsurrounded(int x, int y) {
+    	if(isO(x, y)) {
+    		setUnsurrounded(x, y);
+    		if(x + 1 < width) {
+    			if(isO(x + 1, y)) {
+    				markUnsurrounded(x + 1, y);
+    			}
+    		}
+    		if(x - 1 >= 0) {
+    			if(isO(x - 1, y)) {
+    				markUnsurrounded(x - 1, y);
+    			}
+    		}
+    		if(y + 1 < height) {
+    			if(isO(x, y + 1)) {
+    				markUnsurrounded(x, y + 1);
+    			}
+    		}
+    		if(y - 1 >= 0) {
+    			if(isO(x, y - 1)) {
+    				markUnsurrounded(x, y - 1);
+    			}
+    		}
     	}
-		else {
-			if(isO(x + 1, y)) {
-				surroundedCells = addSurroundedCells(x + 1, y, surroundedCells);
-			}
-			if(!surroundedCells.isEmpty() && isO(x, y + 1)) {
-				surroundedCells = addSurroundedCells(x, y + 1, surroundedCells);
-			}
-		}
-    	return surroundedCells;
     }
-
-    private Collection<Cell> addSurroundedCells(int x, int y, Collection<Cell> surroundedCells) {
-    	Collection<Cell> rightSurroundedCells = surroundedCells(x, y);
-		if(rightSurroundedCells.isEmpty()) {
-			surroundedCells = Collections.emptyList();
-		} else {
-			surroundedCells.addAll(rightSurroundedCells);
-		}
-		return surroundedCells;
-    }
-    private void flip(Cell c) {
-    	board[c.x][c.y] = 'X';
-    }
-	
-	private boolean isBorder(int x, int y) {
-    	return x == 0 || y == 0 || x == width - 1 || y == height - 1;
+    
+    private void setUnsurrounded(int x, int y) {
+    	board[x][y] = 'U';
     }
 
     private boolean isO(int x, int y) {
     	return board[x][y] == 'O';
     }
-
-    private boolean isUnvisited(int x, int y) {
-    	return !visited[x][y];
-    }
-
-    private void visit(int x, int y) {
-    	visited[x][y] = true;
+    
+    private boolean isU(int x, int y) {
+    	return board[x][y] == 'U';
     }
     
-    private static class Cell {
-    	public int x;
-    	public int y;
-    	public Cell(int x, int y) {
-    		this.x = x; 
-    		this.y = y;
-    	}
-    	
-    	public boolean equals(Object c) {
-    		if(c == null) {
-    			return false;
-    		}
-    		if(!(c instanceof Cell)) {
-    			return false;
-    		}
-    		Cell that = (Cell) c;
-    		return this.x == that.x && this.y == that.y;
-    	}
-    	
-    	public int hashCode() {
-    		return String.format("%s,%s", x, y).hashCode();
-    	}
+    private void flip(int x, int y, char c) {
+    	board[x][y] = c;
     }
 }
