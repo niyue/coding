b31b68415eefd071d7a2bed0297102aff16a13cc
Ni Yue <niyue.com@gmail.com>
2013-03-08 23:25:02 +0800

    Partially resolve the 'recover binary tree' problem, cannot reproduce one error reported by leetcode.

---
+++ src/main/java/com/niyue/coding/leetcode/recoverbinarytree/Solution.java
@@ -0,0 +1,81 @@
+package com.niyue.coding.leetcode.recoverbinarytree;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.niyue.coding.leetcode.common.TreeNode;
+
+public class Solution {
+    private TreeNode peak;
+    private TreeNode bottom;
+    private List<TreeNode> queue;
+
+    public void recoverTree(TreeNode root) {
+        if(root != null) {
+            queue = new ArrayList<TreeNode>(3);
+            visit(root);
+            peakOrBottomForLast(queue);
+            swap(peak, bottom);
+        }
+    }   
+
+    private void visit(TreeNode node) {
+        if(node != null) {
+            visit(node.left);
+            enqueue(node);
+            peakOrBottom(queue);
+            visit(node.right);    
+        }
+    }
+
+    private void enqueue(TreeNode node) {
+        queue.add(node);
+        if(queue.size() > 3) {
+            queue.remove(0);
+        }
+    }
+
+    private void peakOrBottom(List<TreeNode> nodes) {
+        if(nodes.size() == 2) {
+            if(nodes.get(0).val > nodes.get(1).val) {
+                peak = nodes.get(0);
+            }
+        } else if(nodes.size() == 3) {
+            if(nodes.get(1).val > nodes.get(0).val && nodes.get(1).val > nodes.get(2).val) {
+            	if(peak == null) {
+            		peak = nodes.get(1);
+            	} else {
+            		peak = peak.val > nodes.get(1).val ? peak : nodes.get(1);
+            	}
+            } else if(nodes.get(1).val < nodes.get(0).val && nodes.get(1).val < nodes.get(2).val) {
+            	if(bottom == null) {
+            		bottom = nodes.get(1);
+            	} else {
+            		bottom = bottom.val < nodes.get(1).val ? bottom : nodes.get(1);
+            	}
+            }
+        }
+    }
+
+    private void peakOrBottomForLast(List<TreeNode> nodes) {
+        if(nodes.size() == 2) {
+            if(nodes.get(1).val < nodes.get(0).val) {
+                bottom = nodes.get(1);
+            }
+        } else if(nodes.size() == 3) {
+            if(nodes.get(2).val < nodes.get(1).val) {
+            	if(bottom == null) {
+            		bottom = nodes.get(2);
+            	} else {
+            		bottom = bottom.val < nodes.get(2).val ? bottom : nodes.get(2);
+            	}
+            }
+        }
+    }
+
+    private void swap(TreeNode one, TreeNode two) {
+        int temp = one.val;
+        one.val = two.val;
+        two.val = temp;
+    }
+}
---
+++ src/test/java/com/niyue/coding/leetcode/recoverbinarytree/RecoverBinaryTreeTest.java
@@ -0,0 +1,65 @@
+package com.niyue.coding.leetcode.recoverbinarytree;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
+
+import org.junit.Test;
+
+import com.niyue.coding.leetcode.common.TreeNode;
+
+public class RecoverBinaryTreeTest {
+
+	@Test
+	// last node cannot be peak 
+	public void test123() {
+		TreeNode root = new TreeNode(1);
+		root.left = new TreeNode(2);
+		root.right = new TreeNode(3);
+		Solution sl = new Solution();
+		sl.recoverTree(root);
+		
+		assertEquals(2, root.val);
+		assertEquals(1, root.left.val);
+		assertEquals(3, root.right.val);
+	}
+	
+	@Test
+	public void testSwitchFirstAndLast() {
+		TreeNode root = new TreeNode(1);
+		root.left = new TreeNode(3);
+		root.left.left = new TreeNode(2);
+		root.left.left.left = new TreeNode(4);
+		
+		Solution sl = new Solution();
+		sl.recoverTree(root);
+		
+		assertEquals(4, root.val);
+		assertEquals(3, root.left.val);
+		assertEquals(2, root.left.left.val);
+		assertEquals(1, root.left.left.left.val);
+	}
+	
+	@Test
+	public void test1324() {
+		TreeNode root = new TreeNode(68);
+		root.left = new TreeNode(41);
+		root.left.left = new TreeNode(-85);
+		root.left.left.left = new TreeNode(-73);
+		root.left.left.right = new TreeNode(-49);
+		root.left.left.left.left = new TreeNode(-98);
+		root.left.left.left.left.left = new TreeNode(-124);
+		
+		Solution sl = new Solution();
+		sl.recoverTree(root);
+		
+		assertEquals(68, root.val);
+		assertEquals(41, root.left.val);
+		assertNull(root.right);
+		assertEquals(-73, root.left.left.val);
+		assertEquals(-85, root.left.left.left.val);
+		assertEquals(-49, root.left.left.right.val);
+		assertEquals(-98, root.left.left.left.left.val);
+		assertEquals(-124, root.left.left.left.left.left.val);
+		
+	}
+}
