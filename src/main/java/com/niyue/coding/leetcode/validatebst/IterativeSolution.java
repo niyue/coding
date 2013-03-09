0f68db24fe5a0a98b0ce99f06daecda1c07f7533
Ni Yue <niyue.com@gmail.com>
2013-03-09 13:19:37 +0800

    Add a link for the iterative tree traversal implementation.

--- src/main/java/com/niyue/coding/leetcode/validatebst/IterativeSolution.java
+++ src/main/java/com/niyue/coding/leetcode/validatebst/IterativeSolution.java
@@ -5,6 +5,9 @@ import java.util.LinkedList;
 
 import com.niyue.coding.leetcode.common.TreeNode;
 
+// http://leetcode.com/onlinejudge#question_98
+// Iterative in order traversal for a tree
+// a simpler in order tree traversal implementation can be found here, http://www.brilliantsheep.com/iterative-binary-tree-traversal-in-java/
 public class IterativeSolution {
     public boolean isValidBST(TreeNode root) {
         boolean isBst = true;
