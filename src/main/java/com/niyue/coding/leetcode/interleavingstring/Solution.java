a2e4a38e72a389b2b82f572394353dfe0139cf8b
Ni Yue <niyue.com@gmail.com>
2013-03-09 19:01:31 +0800

    Remove unnecessary parameter for s3 since s3 = s1 + s2 is invariant.

--- src/main/java/com/niyue/coding/leetcode/interleavingstring/Solution.java
+++ src/main/java/com/niyue/coding/leetcode/interleavingstring/Solution.java
@@ -15,25 +15,26 @@ public class Solution {
         result = new Boolean[s1.length + 1][s2.length + 1];
         boolean isInterleave = false;
         if(s1.length + s2.length == s3.length) {
-            isInterleave = isInterleave(0, 0, 0);
+            isInterleave = isInterleave(0, 0);
         } 
         return isInterleave;
     }
 
     // only length1 * length2 space is required since length3 == length1 + length2 is invariant
-    private boolean isInterleave(int i1, int i2, int i3) {
+    private boolean isInterleave(int i1, int i2) {
         boolean isInterleave = false;
         if(result[i1][i2] != null) {
             isInterleave = result[i1][i2];
         } else {
+        	int i3 = i1 + i2;
             if(i3 < s3.length) {
                 boolean isS1Interleave = false;
                 if(i1 < s1.length && s3[i3] == s1[i1]) {
-                    isS1Interleave = isInterleave(i1 + 1, i2, i3 + 1);
+                    isS1Interleave = isInterleave(i1 + 1, i2);
                 }
                 boolean isS2Interleave = false;
                 if(i2 < s2.length && s3[i3] == s2[i2]) {
-                    isS2Interleave = isInterleave(i1, i2 + 1, i3 + 1);
+                    isS2Interleave = isInterleave(i1, i2 + 1);
                 }
                 isInterleave = isS1Interleave || isS2Interleave;
             } else {
