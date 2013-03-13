0a4995518883c8cdab139ad59f7896af32c908fc
Yue Ni <niyue.com@gmail.com>
2013-03-13 17:44:25 +0800

    Solve 'scramble string' problem in leetcode, an O(n^3) solution using DP, and for each child nodes, verify at least their characters appear the same number of times.

--- src/main/java/com/niyue/coding/leetcode/scramblestring/Solution.java
+++ src/main/java/com/niyue/coding/leetcode/scramblestring/Solution.java
@@ -4,7 +4,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 // http://leetcode.com/onlinejudge#question_87
-// use DP, O(n*n*m) where n is the length of the first string and m is the length of the second string, but it is still too slow to pass the large data set
+// use DP, O(n^3), and add some more checking to ensure at least the child nodes have the same number for each character they have before doing the recursion
 public class Solution {
     private Map<String, Boolean> result;
 
@@ -15,7 +15,7 @@ public class Solution {
 
     private boolean isScramble(char[] s, int ss, int se, char[] t, int ts, int te) {
         boolean isScramble = s[ss] == t[ts];
-        String key = String.format("%s-%s-%s", ss, se, ts);
+        String key = String.format("%s-%s-%s", ss, ts, se - ss);
         if(result.containsKey(key)) {
             isScramble = result.get(key);
         } else {
@@ -37,11 +37,42 @@ public class Solution {
             isScramble = s[ss] == t[ts];
         } else {
             isScramble = 
+            	isCharMatched(s, ss, t, ts, i) && 
+            	isCharMatched(s, ss + i, t, ts + i, se - ss - i + 1) &&
                 isScramble(s, ss, ss + i - 1, t, ts, ts + i - 1) && 
                 isScramble(s, ss + i, se, t, ts + i, te) ||
+                
+                isCharMatched(s, ss, t, te - i + 1, i) && 
+            	isCharMatched(s, ss + i, t, ts, se - ss - i + 1) &&
                 isScramble(s, ss, ss + i - 1, t, te - i + 1, te) && 
                 isScramble(s, ss + i, se, t, ts, te - i);
         }
         return isScramble;    
+    }
+    
+    // for s[ss..ss+length] and t[ts..ts+length], verify if these two strings have the same number of characters for each character
+    private boolean isCharMatched(char[] s, int ss, char[] t, int ts, int length) {
+    	Map<Integer, Integer> charCount = new HashMap<Integer, Integer>();
+    	for(int i = 0; i < length; i++) {
+    		Integer charValue = (int) s[ss+i];
+    		if(!charCount.containsKey(charValue)) {
+    			charCount.put(charValue, 0);
+    		}
+    		charCount.put(charValue, charCount.get(charValue) + 1);
+    	}
+    	boolean matched = true;
+    	for(int i = 0; i < length; i++) {
+    		Integer charValue = (int) t[ts+i];
+    		if(!charCount.containsKey(charValue)) {
+    			matched = false;
+    			break;
+    		} else {
+    			charCount.put(charValue, charCount.get(charValue) - 1);
+    			if(charCount.get(charValue) == 0) {
+    				charCount.remove(charValue);
+    			}
+    		}
+    	}
+    	return matched;
     }
 }
