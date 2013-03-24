048808e86de7dcac48179cc59ecb9d120be34fec
Ni Yue <niyue.com@gmail.com>
2013-03-24 15:48:54 +0800

    Use int array to replace Deque as a stack implementation, and make the program 30% faster and pass the large data set (12 queens), but the algorithm is the still the same.

--- src/main/java/com/niyue/coding/leetcode/nqueensii/Solution.java
+++ src/main/java/com/niyue/coding/leetcode/nqueensii/Solution.java
@@ -1,7 +1,5 @@
 package com.niyue.coding.leetcode.nqueensii;
 
-import java.util.Deque;
-import java.util.LinkedList;
 
 // http://leetcode.com/onlinejudge#question_52
 // a direct variant from n queens problem, too slow to pass the large data set (count solutions for 12 queens)
@@ -11,31 +9,28 @@ public class Solution {
     public int totalNQueens(int n) {
         count = 0;
         N = n;
-        placeQueen(0, 0, new LinkedList<int[]>());
+        placeQueen(0, 0, new int[n]);
         return count;
     }
     
-    private void placeQueen(int row, int col, Deque<int[]> queens) {
+    private void placeQueen(int row, int col, int[] queens) {
         if(row < N && col < N) {
             if(isPlaceable(row, col, queens)) {
-                queens.addFirst(new int[] {row, col});
+            	queens[row] = col;
                 if(row == N - 1) {
                     count++;
                 } else {
                     placeQueen(row + 1, 0, queens);
                 }
-                queens.removeFirst();
             }
             placeQueen(row, col + 1, queens);
         }
     }
 
-    private boolean isPlaceable(int row, int col, Deque<int[]> queens) {
+    private boolean isPlaceable(int row, int col, int[] queens) {
         boolean isPlaceable = true;
-        int diagnoalOne = row + col;
-        int diagnoalTwo = row - col;
-        for(int[] queen : queens) {
-            if(col == queen[1] || diagnoalOne == queen[0] + queen[1] || diagnoalTwo == queen[0] - queen[1]) {
+        for(int i = 0; i < row; i++) {
+        	if(col == queens[i] || row + col == i + queens[i] || row - col == i - queens[i]) {
                 isPlaceable = false;
                 break;
             }
