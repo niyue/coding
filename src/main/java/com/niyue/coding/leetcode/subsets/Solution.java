1f2b444703930e5b2a333f20dca15c57c3bb1753
Yue Ni <niyue.com@gmail.com>
2013-03-25 17:13:05 +0800

    Generate a power set for a set.

---
+++ src/main/java/com/niyue/coding/leetcode/subsets/Solution.java
@@ -0,0 +1,37 @@
+package com.niyue.coding.leetcode.subsets;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+// http://leetcode.com/onlinejudge#question_78
+// generate a power set for a set
+public class Solution {
+    public ArrayList<ArrayList<Integer>> subsets(int[] S) {
+    	Arrays.sort(S);
+    	return subsets(S, 0);
+    }
+
+    private ArrayList<ArrayList<Integer>> subsets(int[] S, int i) {
+    	ArrayList<ArrayList<Integer>> set = new ArrayList<ArrayList<Integer>>();
+        if(i == S.length - 1) {
+        	set.add(new ArrayList<Integer>());
+        	ArrayList<Integer> singleElementSet = new ArrayList<Integer>();
+        	singleElementSet.add(S[i]);
+        	set.add(singleElementSet);
+        } else {
+        	ArrayList<ArrayList<Integer>> subsets = subsets(S, i + 1);
+        	for(ArrayList<Integer> subset : subsets) {
+        		ArrayList<Integer> oneSet = new ArrayList<Integer>();	
+        		oneSet.addAll(subset);
+        		set.add(oneSet);
+        	}
+        	for(ArrayList<Integer> subset : subsets) {
+        		ArrayList<Integer> oneSet = new ArrayList<Integer>();	
+        		oneSet.add(S[i]);
+        		oneSet.addAll(subset);
+        		set.add(oneSet);
+        	}
+        }   
+        return set;     
+    }
+}
