b708335bd6efc9af4bbca25028107edfa255b0b3
Ni Yue <niyue.com@gmail.com>
2013-03-31 22:23:05 +0800

    Solve the longest parentheses validation problem, ran into various issues in different cases.

--- src/main/java/com/niyue/coding/leetcode/longestvalidparentheses/Solution.java
+++ src/main/java/com/niyue/coding/leetcode/longestvalidparentheses/Solution.java
@@ -4,33 +4,50 @@ import java.util.Deque;
 import java.util.LinkedList;
 
 // http://leetcode.com/onlinejudge#question_32
-// initial attempt to solve the parentheses validation, O(n^2) worst case, too slow to pass large data set
+// initial attempt to solve the parentheses validation, O(n)
+// push length and OPEN_PARENTHESES into the same stack
 public class Solution {
+	private static final int OPEN_PARENTHESES = 0;
+	
     public int longestValidParentheses(String s) {
 		char[] chars = s.toCharArray();
 		Deque<Integer> stack = new LinkedList<Integer>();
-		int length = 0;
 		int maxLen = 0;
 		for(int i = 0; i < chars.length; i++) {
 			if(chars[i] == '(') {
-				stack.addFirst(i);
+				stack.addFirst(OPEN_PARENTHESES);
 			} else {
 				if(!stack.isEmpty()) {
-					stack.removeFirst();
-					length += 2;
-					if(length > maxLen) {
-						maxLen = length;
+					int top = stack.removeFirst();
+					if(top == OPEN_PARENTHESES) {
+						if(!stack.isEmpty() && stack.peekFirst() != OPEN_PARENTHESES) {
+							stack.addFirst(stack.removeFirst() + 2);
+						} else {
+							stack.addFirst(2);
+						}
+						if(stack.peekFirst() > maxLen) {
+							maxLen = stack.peekFirst();
+						}
+					} else {
+						if(!stack.isEmpty()) {
+							if(stack.peekFirst() == OPEN_PARENTHESES) {
+								stack.removeFirst();
+								if(!stack.isEmpty() && stack.peekFirst() != OPEN_PARENTHESES) {
+									stack.addFirst(stack.removeFirst() + top + 2);
+								} else {
+									stack.addFirst(top + 2);
+								}
+								if(stack.peekFirst() > maxLen) {
+									maxLen = stack.peekFirst();
+								}
+							} else {
+								stack.clear();
+							}
+						}
 					}
-				} else {
-					length = 0;
 				}
 			}
 		}        
-		if(!stack.isEmpty()) {
-			int lastIndex = stack.removeFirst();
-			maxLen = Math.max(longestValidParentheses(s.substring(0, lastIndex)), 
-					longestValidParentheses(s.substring(lastIndex + 1)));
-		}
 		return maxLen;
     }
 }
--- src/test/java/com/niyue/coding/leetcode/longestvalidparentheses/LongestValidParenthesesTest.java
+++ src/test/java/com/niyue/coding/leetcode/longestvalidparentheses/LongestValidParenthesesTest.java
@@ -16,9 +16,41 @@ public class LongestValidParenthesesTest {
 	
 	@Test
 	// fail to handle this case
+	public void testTwoParentheses() {
+		Solution sl = new Solution();
+		int length = sl.longestValidParentheses("()()");
+		assertEquals(4, length);
+	}
+	
+	@Test
+	// fail to handle this case
 	public void testMultipleParentheses() {
 		Solution sl = new Solution();
 		int length = sl.longestValidParentheses(")()())()()(");
 		assertEquals(4, length);
+	}
+	
+	@Test
+	// fail to handle this case
+	public void testEmpty() {
+		Solution sl = new Solution();
+		int length = sl.longestValidParentheses("");
+		assertEquals(0, length);
+	}
+	
+	@Test
+	// fail to handle this case
+	public void testTwoValid() {
+		Solution sl = new Solution();
+		int length = sl.longestValidParentheses("()(())");
+		assertEquals(6, length);
+	}
+	
+	@Test
+	// fail to handle this case
+	public void testDoubleClosingParentheses() {
+		Solution sl = new Solution();
+		int length = sl.longestValidParentheses("())");
+		assertEquals(2, length);
 	}
 }
