f18a0e920764763cdc8a877514c7867abfa2942b
Ni Yue <niyue.com@gmail.com>
2013-04-06 20:07:14 +0800

    Implement the search in a rotated sorted array.

---
+++ src/main/java/com/niyue/coding/leetcode/searchrotatedsortedarray/Solution.java
@@ -0,0 +1,46 @@
+package com.niyue.coding.leetcode.searchrotatedsortedarray;
+
+// http://leetcode.com/onlinejudge#question_33
+public class Solution {
+    public int search(int[] A, int target) {
+        return search(A, 0, A.length - 1, target);
+    }
+
+    private int search(int[] A, int start, int end, int target) {
+        while(start <= end) {
+            int mid = start + (end - start) / 2;
+            if(A[mid] < target) {
+                if(isLeft(A, mid)) {
+                    start = mid + 1;
+                } else if(isPeak(A, mid)) {
+                    return -1;
+                } else {
+                    int left = search(A, start, mid - 1, target);
+                    int right = search(A, mid + 1, end, target);
+                    return left != -1 ? left : right != -1 ? right : -1;
+                }
+            } else if(A[mid] == target) {
+                return mid;
+            } else {
+                if(isLeft(A, mid) || isPeak(A, mid)) {
+                    int left = search(A, start, mid - 1, target);
+                    int right = search(A, mid + 1, end, target);
+                    return left != -1 ? left : right != -1 ? right : -1;
+                } else {
+                    end = mid - 1;
+                }   
+            }
+        }
+        return -1;      
+    }
+
+    private boolean isLeft(int[] A, int i) {
+        return A[i] > A[0] && A[i] > A[A.length - 1];
+    }
+
+    private boolean isPeak(int[] A, int i) {
+        return A.length > 1 && (i == 0 ? A[i] > A[i + 1] :
+               i == A.length - 1 ? A[i] > A[i - 1] :
+               A[i] > A[i - 1] && A[i] > A[i + 1]);
+    }
+}
