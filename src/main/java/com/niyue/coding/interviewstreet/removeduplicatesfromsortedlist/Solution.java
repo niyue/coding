b69b3f8b7fc3f6988fe402ccaaeb1dc52188afbc
Yue Ni <niyue.com@gmail.com>
2013-04-12 22:29:53 +0800

    Implement deletion of duplicates from a sorted list.

---
+++ src/main/java/com/niyue/coding/interviewstreet/removeduplicatesfromsortedlist/Solution.java
@@ -0,0 +1,22 @@
+package com.niyue.coding.interviewstreet.removeduplicatesfromsortedlist;
+
+import com.niyue.coding.leetcode.common.ListNode;
+
+// http://leetcode.com/onlinejudge#question_83
+public class Solution {
+    public ListNode deleteDuplicates(ListNode head) {
+        if(head != null) {
+            ListNode prev = head;
+            ListNode current = head.next;
+            while(current != null) {
+                if(current.val == prev.val) {
+                    prev.next = current.next;
+                } else {
+                    prev = current;
+                }
+                current = current.next;
+            }
+        }
+        return head;
+    }
+}
