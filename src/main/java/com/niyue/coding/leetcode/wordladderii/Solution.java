fb4d3ea728f32a1bedce752843d1f8498b5ab344
Ni Yue <niyue.com@gmail.com>
2013-04-18 22:15:00 +0800

    Solve the word ladder ii problem in leetcode. It cost me a long time (10 hours I guess) to solve this problem completely. Most of the time, I am stuck in the time limit exceeded issue. The TLE problem got resolved because I realized I should keep a set of minimum paths to a certain word and use combination to find all possible ladders in the end instead of keep all combinations when searching.

--- src/main/java/com/niyue/coding/leetcode/wordladderii/Solution.java
+++ src/main/java/com/niyue/coding/leetcode/wordladderii/Solution.java
@@ -2,7 +2,6 @@ package com.niyue.coding.leetcode.wordladderii;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Deque;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.LinkedList;
@@ -12,6 +11,11 @@ import java.util.Queue;
 import java.util.Set;
 
 // http://leetcode.com/onlinejudge#question_127
+// From start word, compute its connected words (one letter differed words)
+// Use BFS to traverse the implicit graph until end word is found
+// since we need to find all the possible ladders, instead of stopping BFS traversal immediately when the first minimum is found, we just set a flag and continue finding until this level is searched
+// for each word, there may be multiple ladders connected to it, if it is non visited, we need to compute its connected words and do BFS for these words
+// if it is visited previously, we only need to add it to the minimum paths list so that we can get all the paths in the end by using combination
 public class Solution {
 	
 	public ArrayList<ArrayList<String>> findLadders(final String start, final String end, HashSet<String> dict) {
@@ -20,39 +24,40 @@ public class Solution {
     	if(isOneDiff(start, end)) {
     		ladders.add(new ArrayList<String>(Arrays.asList(start, end)));
     	} else {
-    		List<WordNode> solutions = bfs(start, end, dict);
+    		Map<String, List<Ladder>> solutions = bfs(start, end, dict);
     		ladders = ladders(solutions, start, end);
     	}
         return ladders;
     }
     
-    private ArrayList<ArrayList<String>> ladders(List<WordNode> solutions, String start, String end) {
+	// recursively to find all combinational paths to the end word
+    private ArrayList<ArrayList<String>> ladders(Map<String, List<Ladder>> pathMap, String start, String end) {
     	ArrayList<ArrayList<String>> ladders = new ArrayList<ArrayList<String>>();
-    	for(WordNode solution : solutions) {
-			Deque<String> words = words(solution);
-			ladders.add(new ArrayList<String>(words));
-		}
+    	if(end.equals(start)) {
+    		ladders.add(new ArrayList<String>(Arrays.asList(start)));
+    	} else {
+    		if(pathMap.containsKey(end)) {
+    			List<Ladder> paths = pathMap.get(end);
+    			for(Ladder path : paths) {
+    				ArrayList<ArrayList<String>> subLadders = ladders(pathMap, start, path.from.word);
+    				for(ArrayList<String> subLadder : subLadders) {
+    					subLadder.add(end);
+    					ladders.add(subLadder);
+    				}
+    			}
+    		}
+    	}
     	return ladders;
     }
     
-    private Deque<String> words(WordNode wordNode) {
-    	Deque<String> words = new LinkedList<String>();
-    	WordNode current = wordNode;
-    	while(current != null) {
-    		words.addFirst(current.word);
-    		current = current.from;
-    	}
-    	return words;
-    }
-    
-    private static class WordNode {
+    private static class Ladder {
     	public final String word;
-    	public final WordNode from;
-    	private int path = 0;
-    	public WordNode(String word, WordNode from) {
+    	public final Ladder from;
+    	private int length = 0;
+    	public Ladder(String word, Ladder from) {
     		this.word = word;
     		this.from = from;
-    		this.path = from == null ? 1 : 1 + from.path;
+    		this.length = from == null ? 1 : 1 + from.length;
     	}
 		
     	@Override
@@ -61,51 +66,40 @@ public class Solution {
 		}
     }
     
-    private List<WordNode> wordNodes(java.util.Collection<String> startWords, WordNode from, Map<String, Integer> pathMap) {
-    	List<WordNode> wordNodes = new LinkedList<WordNode>();
-    	for(String word : startWords) {
-    		if(!pathMap.containsKey(word) || from.path + 1 <= pathMap.get(word)) {
-    			wordNodes.add(new WordNode(word, from));
-    		}
-    	}
-    	return wordNodes;
-    }
-    
-    private List<WordNode> bfs(String start, String end, Set<String> dict) {
+    private Map<String, List<Ladder>> bfs(String start, String end, Set<String> dict) {
     	dict.add(end);
-    	List<WordNode> solutions = new ArrayList<WordNode>();
-        Queue<WordNode> wordQueue = new LinkedList<WordNode>();
-        Map<String, Integer> pathMap = new HashMap<String, Integer>();
-        wordQueue.add(new WordNode(start, null));
+        Queue<Ladder> wordQueue = new LinkedList<Ladder>();
+        Map<String, List<Ladder>> minLadderMap = new HashMap<String, List<Ladder>>();
+        wordQueue.add(new Ladder(start, null));
         wordQueue.offer(null);
         boolean found = false;
         while(!(wordQueue.size() == 1 && wordQueue.peek() == null)) {
-            WordNode wordNode = wordQueue.poll();
-            if(wordNode == null) {
+            Ladder ladder = wordQueue.poll();
+            if(ladder == null) {
             	if(found) {
             		break;
             	}
                 wordQueue.offer(null);
             } else {
-            	if(end.equals(wordNode.word)) {
-            		found = true;
-            		solutions.add(wordNode);
-            	} else {
-            		if(!pathMap.containsKey(wordNode.word) || wordNode.path <= pathMap.get(wordNode.word)) {
-            			pathMap.put(wordNode.word, wordNode.path);
-            			List<String> connectedWords = transformableWords(wordNode.word, dict);
-            			List<WordNode> childWordNodes = wordNodes(connectedWords, wordNode, pathMap);
-            			wordQueue.addAll(childWordNodes);    
-            		}
-            	}      
+        		if(!minLadderMap.containsKey(ladder.word)) {
+        			minLadderMap.put(ladder.word, new ArrayList<Ladder>(Arrays.asList(ladder)));
+        			if(end.equals(ladder.word)) {
+        				found = true; // instead of finding the first qualified minimum path, we need to find all minimum paths under the same level
+        			} else {
+        				List<Ladder> childWordNodes = transformableWords(ladder, dict, minLadderMap);
+        				wordQueue.addAll(childWordNodes);
+        			}
+        		} else if(ladder.length == minLadderMap.get(ladder.word).get(0).length) {
+        			minLadderMap.get(ladder.word).add(ladder);
+        		}
             }
         }
-        return solutions;
+        return minLadderMap;
     }
     
-    private List<String> transformableWords(String word, Set<String> dict) {
-        List<String> words = new LinkedList<String>();
-        char[] chars = word.toCharArray();
+    private List<Ladder> transformableWords(Ladder from, Set<String> dict, Map<String, List<Ladder>> pathMap) {
+    	List<Ladder> wordNodes = new LinkedList<Ladder>();
+        char[] chars = from.word.toCharArray();
         for(int i = 0;i < chars.length;i++) {
             char ch = chars[i];
             for(char letter : alphabet) {
@@ -113,13 +107,13 @@ public class Solution {
                     chars[i] = letter;
                     String newWord = new String(chars);
                 	if(dict.contains(newWord)) {
-                		words.add(newWord);    
+                		wordNodes.add(new Ladder(newWord, from));    
                 	}
                     chars[i] = ch;
                 }
             }    
         }
-        return words;
+        return wordNodes;
     }
     
     // determine if start and end only differ with one character
