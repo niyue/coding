2cc189f5041559145479b37f8a6342ccfe7e5885
Ni Yue <niyue.com@gmail.com>
2013-04-18 22:34:22 +0800

    Solve the word ladder ii problem in leetcode. It cost me a long time (10 hours I guess) to solve this problem completely. Most of the time, I am stuck in the time limit exceeded issue. The TLE problem got resolved because I realized I should keep a set of minimum paths to a certain word and use combination to find all possible ladders in the end instead of keep all combinations when searching. Some comments are updated in this commit.

--- src/main/java/com/niyue/coding/leetcode/wordladderii/Solution.java
+++ src/main/java/com/niyue/coding/leetcode/wordladderii/Solution.java
@@ -14,14 +14,15 @@ import java.util.Set;
 // From start word, compute its connected words (one letter differed words)
 // Use BFS to traverse the implicit graph until end word is found
 // since we need to find all the possible ladders, instead of stopping BFS traversal immediately when the first minimum is found, we just set a flag and continue finding until this level is searched
-// for each word, there may be multiple ladders connected to it, if it is non visited, we need to compute its connected words and do BFS for these words
-// if it is visited previously, we only need to add it to the minimum paths list so that we can get all the paths in the end by using combination
+// for each word, there may be multiple ladders connected to it, if there is no path to the word previously, we need to compute its connected words and do BFS for these words
+// if the word is visited previously, and if the current path is a minimum path too (it should be equal to the previous path we found to this word, it cannot be smaller than it since we are doing BFS search)
+// in this case, we only need to add it to the minimum paths list so that we can get all the paths in the end by using combination
 public class Solution {
 	
 	public ArrayList<ArrayList<String>> findLadders(final String start, final String end, HashSet<String> dict) {
     	ArrayList<ArrayList<String>> ladders = new ArrayList<ArrayList<String>>();
     	
-    	if(isOneDiff(start, end)) {
+    	if(isOneLetterDiff(start, end)) {
     		ladders.add(new ArrayList<String>(Arrays.asList(start, end)));
     	} else {
     		Map<String, List<Ladder>> solutions = bfs(start, end, dict);
@@ -81,14 +82,16 @@ public class Solution {
             	}
                 wordQueue.offer(null);
             } else {
+            	// if the path to the word is not found yet
         		if(!minLadderMap.containsKey(ladder.word)) {
         			minLadderMap.put(ladder.word, new ArrayList<Ladder>(Arrays.asList(ladder)));
         			if(end.equals(ladder.word)) {
         				found = true; // instead of finding the first qualified minimum path, we need to find all minimum paths under the same level
         			} else {
-        				List<Ladder> childWordNodes = transformableWords(ladder, dict, minLadderMap);
+        				List<Ladder> childWordNodes = transformableWords(ladder, dict);
         				wordQueue.addAll(childWordNodes);
         			}
+        		// if we find another minimum path to the word
         		} else if(ladder.length == minLadderMap.get(ladder.word).get(0).length) {
         			minLadderMap.get(ladder.word).add(ladder);
         		}
@@ -97,7 +100,8 @@ public class Solution {
         return minLadderMap;
     }
     
-    private List<Ladder> transformableWords(Ladder from, Set<String> dict, Map<String, List<Ladder>> pathMap) {
+    // find all one-letter-differed words in the dictionary
+    private List<Ladder> transformableWords(Ladder from, Set<String> dict) {
     	List<Ladder> wordNodes = new LinkedList<Ladder>();
         char[] chars = from.word.toCharArray();
         for(int i = 0;i < chars.length;i++) {
@@ -117,7 +121,7 @@ public class Solution {
     }
     
     // determine if start and end only differ with one character
-    private boolean isOneDiff(String start, String end) {
+    private boolean isOneLetterDiff(String start, String end) {
     	int count = 0;
     	boolean isOneDiff = true;
     	for(int i = 0; i < start.length(); i++) {
