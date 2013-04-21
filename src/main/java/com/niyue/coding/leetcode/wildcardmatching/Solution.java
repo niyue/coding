8b8f095a0f7aea6b7af50fc217cba6dc269be641
Ni Yue <niyue.com@gmail.com>
2013-04-21 17:43:13 +0800

    Solve the wildcard matching problem in leetcode. The recursive version is much easier to understand and implement but it cannot pass the large data set. Eventually solved this problem by following the solution in leetcode discussion by using DP. O(s*p) time, O(s) space.

--- src/main/java/com/niyue/coding/leetcode/wildcardmatching/Solution.java
+++ src/main/java/com/niyue/coding/leetcode/wildcardmatching/Solution.java
@@ -1,56 +1,69 @@
 package com.niyue.coding.leetcode.wildcardmatching;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 // http://leetcode.com/onlinejudge#question_44
-// wildcard matching using memoization to cache the results, but ran into memory limit exceed issue
-// try Map<Integer, Map<Integer, Boolean>> matched but got time limit exceeded issue
+// follow the solution http://discuss.leetcode.com/questions/222/wildcard-matching/435 to solve the large data set
 public class Solution {
-	private Boolean[][] matched; 
     public boolean isMatch(String s, String p) {
-    	char[] pattern = removeContinousStars(p.toCharArray());
-    	matched = new Boolean[s.length() + 1][pattern.length + 1];
-        return isMatch(s.toCharArray(), 0, pattern, 0);
+    	List<Character> pattern = removeContinousStars(p.toCharArray());
+    	boolean isMatch = false;
+    	// this is a must to pass large data set with a case that s and p each is 30k size 
+    	if(s.length() >= pattern.size() - Collections.frequency(pattern, '*')) { 
+    		isMatch = isMatch(s.toCharArray(), asArray(pattern));
+    	}
+    	return isMatch;
     }
     
-    private boolean isMatch(char[] s, int si, char[] p, int pi) {
-    	boolean isMatch = false;
-    	if(si <= s.length && pi <= p.length && matched[si][pi] != null) {
-    		isMatch = matched[si][pi];
-    	} else {
-    		if(pi == p.length) {
-    			isMatch = si == s.length;
-    		} else {
-    			if(p[pi] == '?') {
-    				isMatch = isMatch(s, si + 1, p, pi + 1);
-    			} else if(p[pi] == '*') {
-    				for(int i = 0; i <= s.length - si; i++) {
-    					if(isMatch(s, si + i, p, pi + 1)) {
-    						isMatch = true;
-    						break;
-    					}
-    				}
+    private boolean isMatch(char[] s, char[] p) {
+    	boolean[] prev = null;
+    	boolean[] current = null;
+    	for(int pi = p.length; pi >= 0; pi--) {
+    		current = new boolean[s.length + 1];
+    		for(int si = s.length; si >= 0; si--) {
+    			if(pi == p.length) {
+    				current[si] = si == s.length;
+    			} else if(si == s.length) {
+    				current[si] = p[pi] == '*' && prev[si];
     			} else {
-    				if(si < s.length) {
-    					isMatch = s[si] == p[pi] ? isMatch(s, si + 1, p, pi + 1) : false;
+    				if(p[pi] != '*' && p[pi] != '?') {
+    					current[si] = s[si] == p[pi] && prev[si + 1];
+    				} else if(p[pi] == '?') {
+    					current[si] = prev[si + 1];
+    				} else {
+    					current[si] = hasTrue(prev, si) || hasTrue(current, si + 1);
     				}
     			}
     		}
-    		if(si <= s.length && pi <= p.length) {
-    			matched[si][pi] = isMatch;
-    		}
+    		prev = current;
     	}
-        return isMatch;
+        return current[0];
     }
     
-    private char[] removeContinousStars(char[] p) {
+    private boolean hasTrue(boolean[] matches, int start) {
+    	boolean hasTrue = false;
+    	for(int i = start; i < matches.length; i++) {
+    		if(matches[i]) {
+    			hasTrue = true;
+    			break;
+    		}
+    	}
+    	return hasTrue;
+    }
+    
+    private List<Character> removeContinousStars(char[] p) {
     	List<Character> pattern = new ArrayList<Character>();
     	for(int i = 0; i < p.length; i++) {
 			if(p[i] != '*' || pattern.isEmpty() || pattern.get(pattern.size() - 1) != '*') {
 				pattern.add(p[i]);
 			}
     	}
+    	return pattern;
+    }
+    
+    private char[] asArray(List<Character> pattern) {
     	char[] pp = new char[pattern.size()];
     	for(int i = 0; i < pp.length; i++) {
     		pp[i] = pattern.get(i);
--- src/test/java/com/niyue/coding/leetcode/wildcardmatching/WildCardMatchingTest.java
+++ src/test/java/com/niyue/coding/leetcode/wildcardmatching/WildCardMatchingTest.java
@@ -1,6 +1,7 @@
 package com.niyue.coding.leetcode.wildcardmatching;
 
 import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 
@@ -9,5 +10,18 @@ public class WildCardMatchingTest {
 	public void testZeroLengthString() {
 		Solution sl = new Solution();
 		assertFalse(sl.isMatch("", "?"));
+	}
+	
+	@Test
+	// forget to reinitialize current variable in inner loop
+	public void testDoubleA() {
+		Solution sl = new Solution();
+		assertTrue(sl.isMatch("aa", "aa"));
+	}
+	
+	@Test
+	public void testLongStringMatching() {
+		Solution sl = new Solution();
+		assertFalse(sl.isMatch("aaabbbbaaaabbabbbbaabbabaaababaababaaaaaaabaaababbaababbaababbbaaaaabaabbabbaabaababbaabababbbbbbbbabbaabbaaabaababaabaababababababbbbbbabbabbaabbaabaaaaaababaabbbaaabaaabbbbbbbbbaabaabaaabaaabbabbabb", "****a*b*b**b*bbb*b**bba***b**b*b*b**ba***b*b*a*b*b*****a**aaa*baaa*ba*****a****ba*a****a**b*******a*a"));
 	}
 }
