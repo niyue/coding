e9aa24f1a9c292c581e9938e9ab447b12f35d274
Yue Ni <niyue.com@gmail.com>
2013-05-31 09:06:23 +0800

    Implement retrieval of the longest increasing sequence instead of only its length.

--- src/main/java/com/niyue/coding/careercup/longestincreasingseq/LongestIncreasingSeq.java
+++ src/main/java/com/niyue/coding/careercup/longestincreasingseq/LongestIncreasingSeq.java
@@ -1,43 +1,61 @@
 package com.niyue.coding.careercup.longestincreasingseq;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Deque;
+import java.util.LinkedList;
 import java.util.List;
 
 // count the length of longest increasing subsequence
 // http://www.careercup.com/question?id=15499786
 public class LongestIncreasingSeq {
-	public int lis(List<Integer> seq) {
-		List<Integer> max = new ArrayList<Integer>();
+	public List<Integer> lis(List<Integer> seq) {
+		List<Deque<Node>> max = new ArrayList<Deque<Node>>();
 		int length = 0;
 		for(int i = 0; i < seq.size(); i++) {
-			if(i == 0) {
-				max.add(i);
+			int n = seq.get(i);
+			int pos = binarySearch(max, seq, n);
+			Node prev = pos == 0 ? null : max.get(pos - 1).peekFirst();
+			Node maxNode = new Node(i, prev);
+			if(pos > length - 1) {
+				Deque<Node> nodes = new LinkedList<Node>();
+				nodes.add(maxNode);
+				max.add(nodes);
 				length++;
 			} else {
-				int n = seq.get(i);
-				int pos = binarySearch(max, seq, n);
-				if(pos > length - 1) {
-					max.add(i);
-					length++;
-				} else {
-					max.set(pos, i);
-				}
+				max.get(pos).addFirst(maxNode);
 			}
 		}
-		return length;
+		List<Integer> lis = new ArrayList<Integer>();
+		Node maxLengthNode = max.get(max.size() - 1).peekFirst();
+		while(maxLengthNode != null) {
+			lis.add(seq.get(maxLengthNode.value));
+			maxLengthNode = maxLengthNode.prev;
+		}
+		Collections.reverse(lis);
+		return lis;
 	}
 	
-	private int binarySearch(List<Integer> max, List<Integer> seq, int n) {
+	private int binarySearch(List<Deque<Node>> max, List<Integer> seq, int n) {
 		int start = 0;
 		int end = max.size() - 1;
 		while(start <= end) {
 			int mid = start + (end - start) / 2;
-			if(seq.get(max.get(mid)) < n) {
+			if(seq.get(max.get(mid).peekFirst().value) < n) {
 				start = mid + 1;
 			} else {
 				end = mid - 1;
 			}
 		}
 		return start;
+	}
+	
+	private static class Node {
+		public final int value;
+		public final Node prev;
+		public Node(int value, Node prev) {
+			this.value = value;
+			this.prev = prev;
+		}
 	}
 }
--- src/test/java/com/niyue/coding/careercup/longestincreasingseq/LongestIncreasingSeqTest.java
+++ src/test/java/com/niyue/coding/careercup/longestincreasingseq/LongestIncreasingSeqTest.java
@@ -1,8 +1,10 @@
 package com.niyue.coding.careercup.longestincreasingseq;
 
-import static org.junit.Assert.assertEquals;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
 
 import java.util.Arrays;
+import java.util.List;
 
 import org.junit.Test;
 
@@ -11,49 +13,56 @@ public class LongestIncreasingSeqTest {
 	@Test
 	public void testSingle() {
 		LongestIncreasingSeq lis = new LongestIncreasingSeq();
-		int length = lis.lis(Arrays.asList(1));
-		assertEquals(1, length);
+		List<Integer> seq = lis.lis(Arrays.asList(1));
+		assertThat(seq, is(Arrays.asList(1)));
 	}
 	
 	@Test
 	public void testTwoContinuous() {
 		LongestIncreasingSeq lis = new LongestIncreasingSeq();
-		int length = lis.lis(Arrays.asList(1, 2));
-		assertEquals(2, length);
+		List<Integer> seq = lis.lis(Arrays.asList(1, 2));
+		assertThat(seq, is(Arrays.asList(1, 2)));
 	}
 	
 	@Test
 	public void testThreeContinuous() {
 		LongestIncreasingSeq lis = new LongestIncreasingSeq();
-		int length = lis.lis(Arrays.asList(1, 2, 3));
-		assertEquals(3, length);
+		List<Integer> seq = lis.lis(Arrays.asList(1, 2, 3));
+		assertThat(seq, is(Arrays.asList(1, 2, 3)));
 	}
 	
 	@Test
 	public void testThree() {
 		LongestIncreasingSeq lis = new LongestIncreasingSeq();
-		int length = lis.lis(Arrays.asList(1, 3, 2));
-		assertEquals(2, length);
+		List<Integer> seq = lis.lis(Arrays.asList(1, 3, 2));
+		assertThat(seq, is(Arrays.asList(1, 2)));
 	}
 	
 	@Test
 	public void testAllDecreasing() {
 		LongestIncreasingSeq lis = new LongestIncreasingSeq();
-		int length = lis.lis(Arrays.asList(3, 2, 1, 0));
-		assertEquals(1, length);
+		List<Integer> seq = lis.lis(Arrays.asList(3, 2, 1, 0));
+		assertThat(seq, is(Arrays.asList(0)));
 	}
 	
 	@Test
 	public void testUpDown() {
 		LongestIncreasingSeq lis = new LongestIncreasingSeq();
-		int length = lis.lis(Arrays.asList(1, 2, 3, 0));
-		assertEquals(3, length);
+		List<Integer> seq = lis.lis(Arrays.asList(1, 2, 3, 0));
+		assertThat(seq, is(Arrays.asList(1, 2, 3)));
 	}
 	
 	@Test
 	public void testUpDownUp() {
 		LongestIncreasingSeq lis = new LongestIncreasingSeq();
-		int length = lis.lis(Arrays.asList(1, 2, 3, 0, 5));
-		assertEquals(4, length);
+		List<Integer> seq = lis.lis(Arrays.asList(1, 2, 3, 0, 5));
+		assertThat(seq, is(Arrays.asList(1, 2, 3, 5)));
+	}
+	
+	@Test
+	public void testVanDerCorputSeq() {
+		LongestIncreasingSeq lis = new LongestIncreasingSeq();
+		List<Integer> seq = lis.lis(Arrays.asList(0, 8, 4, 12, 2, 10, 6, 14, 1, 9, 5, 13, 3, 11, 7, 15));
+		assertThat(seq, is(Arrays.asList(0, 2, 6, 9, 11, 15)));
 	}
 }
