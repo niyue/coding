0f65004db71d51c62c7318c420cc3bfa4f1b15a3
Ni Yue <niyue.com@gmail.com>
2013-06-01 22:37:40 +0800

    Simplified the sub sequence generation for LCS problem, the LCS can be computed from just the length table and s1 string.

--- src/main/java/com/niyue/coding/careercup/longestcommonseq/LongestCommonSeq.java
+++ src/main/java/com/niyue/coding/careercup/longestcommonseq/LongestCommonSeq.java
@@ -2,54 +2,50 @@ package com.niyue.coding.careercup.longestcommonseq;
 
 
 // longest common subsequence problem
-// bottom up approach DP to calculate both the length and LCS string
+// bottom up approach DP to calculate the length of LCS
 public class LongestCommonSeq {
 	public String lcs(String s1, String s2) {
 		return lcs(s1.toCharArray(), s2.toCharArray());
 	}
 	
 	private String lcs(char[] s1, char[] s2) {
-		Char[][] lcs = new Char[s1.length][s2.length];
+		int[][] lcs = new int[s1.length][s2.length];
 		for(int i = 0; i < s1.length; i++) {
 			for(int j = 0; j < s2.length; j++) {
 				if(i == 0 && j == 0) {
-					lcs[i][j] = s1[i] == s2[j] 
-							? new Char(-1, -1, s1[i], 1) 
-							: new Char(-1, -1, Character.MIN_VALUE, 0);
+					lcs[i][j] = s1[i] == s2[j] ? 1 : 0;
 				} else if(i == 0 || j == 0) { 
 					lcs[i][j] = s1[i] == s2[j] 
-							? new Char(-1, -1, s1[i], 1) 
+							? 1 
 							: i == 0 ? lcs[i][j - 1] : lcs[i - 1][j];
 				} else {
 					lcs[i][j] = s1[i] == s2[j] 
-							? new Char(i - 1, j - 1, s1[i], lcs[i - 1][j - 1].length + 1)
-							: (lcs[i - 1][j].length > lcs[i][j - 1].length 
-								? new Char(i - 1, j, Character.MIN_VALUE, lcs[i - 1][j].length)
-								: new Char(i, j - 1, Character.MIN_VALUE, lcs[i][j - 1].length));
+							? lcs[i - 1][j - 1] + 1
+							: Math.max(lcs[i - 1][j], lcs[i][j - 1]); 
 				}
 			}
 		}
-		Char current = lcs[s1.length - 1][s2.length - 1];
 		StringBuilder seq = new StringBuilder();
-		while(current != null && current.length > 0) {
-			if(Character.MIN_VALUE != current.c) {
-				seq.append(current.c);
+		int x = s1.length - 1;
+		int y = s2.length - 1;
+		while(x >= 0 && y >= 0 && lcs[x][y] > 0) {
+			if(x == 0) {
+				seq.append(s1[x]);
+				break;
+			} else if(y == 0) {
+				seq.append(s2[y]);
+				break;
 			}
-			current = current.x >= 0 && current.y >= 0 ? lcs[current.x][current.y] : null;
+			if(lcs[x][y] == lcs[x - 1][y]) {
+				x--;
+			} else if(lcs[x][y] == lcs[x][y - 1]) {
+				y--;
+			} else {
+				seq.append(s1[x]);
+				x--;
+				y--;
+			}
 		}
 		return seq.reverse().toString();
-	}
-	
-	private static class Char {
-		public final int x;
-		public final int y;
-		public final char c;
-		public final int length;
-		public Char(int x, int y, char c, int length) {
-			this.x = x;
-			this.y = y;
-			this.c = c;
-			this.length = length;
-		}
 	}
 }
