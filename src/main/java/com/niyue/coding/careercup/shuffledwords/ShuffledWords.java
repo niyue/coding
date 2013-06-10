2bd59cf247a5d4368e15c6930af3ee34079e7d1f
Ni Yue <niyue.com@gmail.com>
2013-06-10 23:02:27 +0800

    Improve restoring sentence efficiency with top down DP, O(n^2) time, O(n) space.

--- src/main/java/com/niyue/coding/careercup/shuffledwords/ShuffledWords.java
+++ src/main/java/com/niyue/coding/careercup/shuffledwords/ShuffledWords.java
@@ -1,55 +1,65 @@
 package com.niyue.coding.careercup.shuffledwords;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.LinkedHashSet;
-import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
 // http://www.careercup.com/question?id=15070901
 // only characters within a word are shuffled but they are not shuffled across different words
 public class ShuffledWords {
+	private Integer[] result;
+	private static final int NO_SOLUTION = -1;
 	public String restore(String shuffledWords, Set<String> dict) {
+		result = new Integer[shuffledWords.length()];
 		Map<String, Set<String>> anagramSet = anagramSet(dict);
-		List<String> words = separate(shuffledWords, 0, anagramSet);
-		String sentence = join(words, anagramSet);
+		Integer nextWord = separate(shuffledWords, 0, anagramSet);
+		String sentence = join(shuffledWords, nextWord, anagramSet);
 		return sentence;
 	}
 	
-	private String join(List<String> words, Map<String, Set<String>> anagramSet) {
+	private String join(String shuffledWord, Integer nextWord, Map<String, Set<String>> anagramSet) {
 		StringBuilder s = new StringBuilder();
-		for(String word : words) {
-			String anagramWord = anagramSet.get(word).iterator().next(); 
+		Integer prev = 0;
+		while(nextWord != null && nextWord <= shuffledWord.length()) {
+			String word = shuffledWord.substring(prev, nextWord);
+			String anagram = sortedAnagram(word);
+			String anagramWord = anagramSet.get(anagram).iterator().next(); 
 			if(s.length() == 0) {
 				s.append(anagramWord);
 			} else {
 				s.append(" ").append(anagramWord);
 			}
+			prev = nextWord;
+			nextWord = nextWord < shuffledWord.length() ? result[nextWord] : null;
 		}
 		return s.toString();
 	}
 	
-	private List<String> separate(String shuffledWords, int start, Map<String, Set<String>> anagramSet) {
-		List<String> separatedWords = new ArrayList<String>();
-		for(int i = start; i < shuffledWords.length(); i++) {
-			String word = shuffledWords.substring(start, i + 1);
-			String anagram = sortedAnagram(word);
-			if(anagramSet.containsKey(anagram)) {
-				if(i + 1 == shuffledWords.length()) {
-					separatedWords.add(anagram);
-				} else {
-					List<String> words = separate(shuffledWords, i + 1, anagramSet);
-					if(!words.isEmpty()) {
-						separatedWords.add(anagram);
-						separatedWords.addAll(words);
-						break;
+	private int separate(String shuffledWords, int start, Map<String, Set<String>> anagramSet) {
+		Integer nextWord = NO_SOLUTION;
+		if(result[start] != null) {
+			nextWord = result[start];
+		} else {
+			for(int i = start; i < shuffledWords.length(); i++) {
+				String word = shuffledWords.substring(start, i + 1);
+				String anagram = sortedAnagram(word);
+				if(anagramSet.containsKey(anagram)) {
+					if(i + 1 == shuffledWords.length()) {
+						nextWord = i + 1;
+					} else {
+						Integer next = separate(shuffledWords, i + 1, anagramSet);
+						if(next != NO_SOLUTION) {
+							nextWord = i + 1;
+							break;
+						}
 					}
 				}
 			}
+			result[start] = nextWord;
 		}
-		return separatedWords;
+		return nextWord;
 	}
 	
 	private String sortedAnagram(String word) {
