0379dccd879cd10f6f090404994b4608fd436976
Yue Ni <niyue.com@gmail.com>
2013-06-19 18:05:21 +0800

    Incomplete solution for finding median of two sorted arrays.

--- src/main/java/com/niyue/coding/leetcode/medianoftwosortedarrays/Solution.java
+++ src/main/java/com/niyue/coding/leetcode/medianoftwosortedarrays/Solution.java
@@ -1,32 +1,62 @@
 package com.niyue.coding.leetcode.medianoftwosortedarrays;
 
+
 public class Solution {
     public double findMedianSortedArrays(int A[], int B[]) {
-        return median(A, 0, A.length - 1, B, 0, B.length - 1);           
+        return findMedian(A, 0, A.length - 1, B, 0, B.length - 1);
     }
-
-    private double median(int[] A, int startA, int endA, int B[], int startB, int endB) {
+    
+    private double findMedian(int[] A, int startA, int endA, int[] B, int startB, int endB) {
+        double median = 0;
+        int lengthA = endA - startA + 1;
+        int lengthB = endB - startB + 1;
         int midA = startA + (endA - startA) / 2;
         int midB = startB + (endB - startB) / 2;
-        double median = 0;
-        if(startA > endA) {
-            median = median(B, startB, endB);
-        } else if(startB > endB) {
-            median = median(A, startA, endA);
-        } else if(startA == endA || startB == endB) {
+        if(lengthA == 1 && lengthB == 1) {
             median = (A[midA] + B[midB]) / 2.0;
+        } else if(lengthA == 0) {
+            median = median(B, startB, endB);
+        } else if(lengthB == 0) {
+            median = median(A, startA, endA);
         } else {
-            if(A[midA] >= B[midB]) {
-                median = median(A, startA, midA, B, midB, endB);
+            if(A[midA] == B[midB]) {
+            	boolean hasOddLength = lengthA % 2 != 0 || lengthB % 2 != 0;
+            	if(hasOddLength) {
+            		median = A[midA];
+            	} else {
+            		median = (A[midA] + Math.min(A[midA + 1], B[midB + 1])) / 2.0;
+            	}
+            } else if(A[midA] < B[midB]) {
+            	int firstHalfLengthA = (lengthA - 1) / 2;
+            	int secondHalfLengthB = (lengthB + 1) / 2;
+        		if(firstHalfLengthA < secondHalfLengthB) {
+        			median = findMedian(A, midA, endA, B, startB, endB - firstHalfLengthA);    
+        		} else {
+        			median = findMedian(A, startA + secondHalfLengthB, endA, B, startB, midB);
+        		}
             } else {
-                median = median(A, midA, endA, B, startB, midB);
-            }
+            	int firstHalfLengthB = (lengthB - 1) / 2;
+            	int secondHalfLengthA = (lengthA + 1) / 2;
+            	if(firstHalfLengthB < secondHalfLengthA) {
+            		median = findMedian(A, startA, endA - firstHalfLengthB, B, midB, endB);    
+            	} else {
+            		median = findMedian(A, startA, midA, B, startB + secondHalfLengthA, endB);
+            	}
+            }    
+        }
+       
+        return median;
+    }    
+
+    private double median(int[] X, int start, int end) {
+        boolean isEven = (end - start + 1) % 2 == 0;
+        int mid = start + (end - start) / 2;
+        double median = 0;
+        if(isEven) {
+            median = (X[mid] + X[mid + 1]) / 2.0;
+        } else {
+            median = X[mid];
         }
         return median;
-    }
-
-    private double median(int[] C, int start, int end) {
-        int mid = start + (end - start) / 2;
-        return (end - start) % 2 == 0 ? C[mid] : (C[mid] + C[mid + 1]) / 2.0;
     }
 }
--- src/test/java/com/niyue/coding/leetcode/medianoftwosortedarrays/MedianOfTwoSortedArraysTest.java
+++ src/test/java/com/niyue/coding/leetcode/medianoftwosortedarrays/MedianOfTwoSortedArraysTest.java
@@ -15,9 +15,40 @@ public class MedianOfTwoSortedArraysTest {
 	}
 	
 	@Test
-	public void testTwoAndTwo() {
+	// isEven is not correctly calculated
+	public void testTwoSameArrays() {
 		Solution sl = new Solution();
 		double median = sl.findMedianSortedArrays(new int[] {1, 2}, new int[] {1, 2});
 		assertEquals(1.5, median, 0.0001);
+	}
+	
+	@Test
+	// if both A and B has even length, when A[mid] == B[mid], this case is not handled correctly
+	public void testTwoDifferentArrays() {
+		Solution sl = new Solution();
+		double median = sl.findMedianSortedArrays(new int[] {1, 1}, new int[] {1, 2});
+		assertEquals(1, median, 0.0001);
+	}
+	
+	@Test
+	public void testTwoMedianInSameArray() {
+		Solution sl = new Solution();
+		double median = sl.findMedianSortedArrays(new int[] {5, 6}, new int[] {3, 7});
+		assertEquals(5.5, median, 0.0001);
+	}
+	
+
+	@Test
+	public void testLargerThanFour() {
+		Solution sl = new Solution();
+		double median = sl.findMedianSortedArrays(new int[] {1, 2}, new int[] {1, 2, 3});
+		assertEquals(2, median, 0.0001);
+	}
+	
+	@Test
+	public void testEvenLengthAndOddLength() {
+		Solution sl = new Solution();
+		double median = sl.findMedianSortedArrays(new int[] {6, 7}, new int[] {1, 2, 3, 4, 5, 8, 9});
+		assertEquals(5, median, 0.0001);
 	}
 }
