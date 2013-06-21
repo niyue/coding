de88b58980e8b47e2ff950ab3bbc669254dfb15a
Yue Ni <niyue.com@gmail.com>
2013-06-21 15:54:50 +0800

    Instead of relying on JDK API, implement an in-place version of reversion using a simple state machine.

--- src/main/java/com/niyue/coding/careercup/reversestringwords/ReverseStringWords.java
+++ src/main/java/com/niyue/coding/careercup/reversestringwords/ReverseStringWords.java
@@ -1,20 +1,54 @@
 package com.niyue.coding.careercup.reversestringwords;
 
 // http://www.geeksforgeeks.org/reverse-words-in-a-given-string/
+// An inplace implementation that uses a simple state machine to scan through the characters
 public class ReverseStringWords {
+	private final int NOT_IN_WORD = -1;
+	private final int IN_WORD = 1;
+	
 	public String reverse(String string) {
-		String[] words = string.split(" ");
-		StringBuilder builder = new StringBuilder();
-		for(int i = 0; i < words.length; i++) {
-			if(i > 0) {
-				builder.append(" ");
-			}
-			builder.append(reverseWord(words[i]));
-		}
-		return reverseWord(builder.toString());
+		char[] chars = string.toCharArray();
+		char[] reversedChars = reverse(chars);
+		return new String(reversedChars);
 	}
 	
-	private String reverseWord(String word) {
-		return new StringBuilder(word).reverse().toString();
+	private char[] reverse(char[] chars) {
+		int start = 0;
+		int end = 0;
+		int status = NOT_IN_WORD;
+		for(int i = 0; i < chars.length; i++) {
+			if(status == NOT_IN_WORD) {
+				if(!Character.isWhitespace(chars[i])) {
+					status = IN_WORD;
+					start = i;
+				}
+			} else {
+				if(Character.isWhitespace(chars[i])) {
+					status = NOT_IN_WORD;
+					end = i - 1;
+					reverse(chars, start, end);
+				}
+			}
+		}
+		
+		if(status == IN_WORD) {
+			reverse(chars, start, chars.length - 1);
+		}
+		
+		return reverse(chars, 0, chars.length - 1);
+	}
+	
+	private char[] reverse(char[] chars, int start, int end) {
+		int mid = start + (end - start) / 2;
+		for(int i = start; i <= mid; i++) {
+			swap(chars, i, end - i + start);
+		}
+		return chars;
+	}
+	
+	private void swap(char[] chars, int i, int j) {
+		char temp = chars[i];
+		chars[i] = chars[j];
+		chars[j] = temp;
 	}
 }
--- src/test/java/com/niyue/coding/careercup/reversestringwords/ReverseStringWordsTest.java
+++ src/test/java/com/niyue/coding/careercup/reversestringwords/ReverseStringWordsTest.java
@@ -14,10 +14,52 @@ public class ReverseStringWordsTest {
 	}
 	
 	@Test
+	public void testMultipleSpaces() {
+		ReverseStringWords rev = new ReverseStringWords();
+		String reversed = rev.reverse("001  002");
+		assertThat(reversed, is("002  001"));
+	}
+	
+	@Test
+	public void testTrailingSpace() {
+		ReverseStringWords rev = new ReverseStringWords();
+		String reversed = rev.reverse("001  ");
+		assertThat(reversed, is("  001"));
+	}
+	
+	@Test
+	public void testTwoWordsTrailingSpace() {
+		ReverseStringWords rev = new ReverseStringWords();
+		String reversed = rev.reverse("001 002  ");
+		assertThat(reversed, is("  002 001"));
+	}
+	
+	@Test
 	public void testMultipleWords() {
 		ReverseStringWords rev = new ReverseStringWords();
 		String reversed = rev.reverse("My name is X Y Z");
 		assertThat(reversed, is("Z Y X is name My"));
+	}
+	
+	@Test
+	public void testLeadingSpace() {
+		ReverseStringWords rev = new ReverseStringWords();
+		String reversed = rev.reverse("  001");
+		assertThat(reversed, is("001  "));
+	}
+	
+	@Test
+	public void testLeadingSpacesTwoWords() {
+		ReverseStringWords rev = new ReverseStringWords();
+		String reversed = rev.reverse("  001 002");
+		assertThat(reversed, is("002 001  "));
+	}
+	
+	@Test
+	public void testTabAsWhiteSpace() {
+		ReverseStringWords rev = new ReverseStringWords();
+		String reversed = rev.reverse("001	002");
+		assertThat(reversed, is("002	001"));
 	}
 	
 	@Test
