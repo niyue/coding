6a4c6a3c7358612033e63bf839b4ffa24bc83414
Yue Ni <niyue.com@gmail.com>
2013-06-24 14:26:04 +0800

    Solve the sukodu solver problem in leetcode using back tracking, this version is still too slow to pass the leetcode online judge.

---
+++ src/main/java/com/niyue/coding/leetcode/sudokusolver/Solution.java
@@ -0,0 +1,212 @@
+package com.niyue.coding.leetcode.sudokusolver;
+
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.Deque;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.PriorityQueue;
+import java.util.Queue;
+import java.util.Set;
+
+// http://leetcode.com/onlinejudge#question_37
+// Solve a sukodu puzzle using back tracking
+// use a heap to order the cell to be filled according to the possible numbers allowed in a cell initially
+// try each number allowed in a cell and verifies the result, if the number can be used, move on to next cell, otherwise, restore the board to previous status and backtrack
+// This version is too slow to pass even the small data set in leetcode, two tests can pass in my machine with 200ms but it is considered too slow in leetcode online judge
+public class Solution {
+
+    public void solveSudoku(char[][] board) {
+        Cell[][] grids = new Cell[board.length][];
+        Queue<Cell> fillingOrder = cellQueue(board, grids);
+        Deque<Cell> stack = new ArrayDeque<Cell>(fillingOrder);
+        Deque<Cell> solution = new ArrayDeque<Cell>(stack.size());
+        while(!stack.isEmpty()) {
+            Cell c = stack.peekFirst();
+            int nextAllow = c.nextAllow();
+            if(nextAllow == 10) {
+                if(!solution.isEmpty()) {
+                    // no solution can be found, backtrack
+                    Cell prev = solution.removeFirst(); 
+  
+                    for(Cell uc : prev.lastUpdatedCells) {
+                        uc.allow(prev.value);
+                    }
+                    prev.reset();
+                    c.clearLastTry();
+                    stack.addFirst(prev);
+                } else {
+                    // no solution can be found for this layout
+                    break;
+                }
+            } else {
+                c = stack.removeFirst();
+                c.value = nextAllow;
+                c.step = solution.size();
+                solution.addFirst(c);
+
+                updateDisallows(grids, c);
+            }        
+        }
+
+        for(Cell c : solution) {
+            board[c.x][c.y] = (char) (c.value + '0');
+        }
+    }
+    
+    private Queue<Cell> cellQueue(char[][] board, Cell[][] grids) {
+    	Queue<Cell> queue = new PriorityQueue<Cell>(81, new CellComparator());
+        for(int i = 0; i < board.length; i++) {
+            grids[i] = new Cell[board[i].length];
+            for(int j = 0; j < board[i].length; j++) {
+                if(board[i][j] == '.') {
+                    grids[i][j] = new Cell(i, j);
+                } else {
+                    grids[i][j] = new Cell(i, j, board[i][j] - '0');   
+                }
+            }
+        }
+
+        for(int i = 0; i < board.length; i++) {
+            for(int j = 0; j < board[i].length; j++) {
+                if(!grids[i][j].isPreDefined()) {
+                    List<Cell> affectedCells = affectedCells(grids, i, j, true);
+                    grids[i][j].disallow(affectedCells);
+                }
+            }
+        }
+
+        for(int i = 0; i < board.length; i++) {
+            for(int j = 0; j < board[i].length; j++) {
+                if(!grids[i][j].isPreDefined()) {
+                    queue.offer(grids[i][j]);
+                }
+            }
+        }
+        return queue;
+    }
+
+    private List<Cell> affectedCells(Cell[][] grids, int i, int j, boolean includePredefined) {
+        List<Cell> affectedCells = new ArrayList<Cell>(27);
+        for(int x = 0; x < grids.length; x++) {
+            if((includePredefined || !grids[x][j].isPreDefined()) && x != i) {
+                affectedCells.add(grids[x][j]);
+            }
+        }
+        
+        for(int y = 0; y < grids[0].length; y++) {
+            if((includePredefined || !grids[i][y].isPreDefined()) && y != j) {
+                affectedCells.add(grids[i][y]);
+            }
+        }
+
+        int xShift = i % 3;
+        int yShift = j % 3;
+        for(int x = 0; x < 3; x++) {
+            for(int y = 0; y < 3; y++) {
+                if(x != xShift && y != yShift) {
+                    Cell c = grids[i + x - xShift][j + y - yShift];
+                    if(includePredefined || !c.isPreDefined()) {
+                        affectedCells.add(c);
+                    }
+                }
+            }
+        }
+        return affectedCells;    
+    }
+
+    private void updateDisallows(Cell[][] grids, Cell c) {
+        List<Cell> affectedCells = affectedCells(grids, c.x, c.y, false);
+
+        for(Cell affectedCell : affectedCells) {
+            affectedCell.disallow(c.value);
+            c.lastUpdatedCells.add(affectedCell);
+        }
+    }
+
+    private static class Cell {
+        private static final Integer PREDEFINED = -1;
+        public final int x;
+        public final int y;
+        public Integer value = null;
+        public Integer step = null;
+        public int lastTry = 0;
+        public Set<Cell> lastUpdatedCells = new HashSet<Cell>();
+        public Map<Integer, Integer> disallows = new HashMap<Integer, Integer>();
+        public Cell(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+
+        public Cell(int x, int y, int value) {
+            this(x, y);
+            this.value = value;
+            this.step = PREDEFINED;
+        }
+
+        public boolean isPreDefined() {
+            return step == PREDEFINED;
+        }
+
+        public int nextAllow() {
+            while(lastTry < 10) {
+                lastTry++;
+                if(!disallows.containsKey(lastTry)) {
+                    break;
+                }
+            }
+            return lastTry;
+        }
+        
+        public void clearLastTry() {
+        	this.lastTry = 0;
+        }
+
+        public void disallow(int v) {
+            if(!disallows.containsKey(v)) {
+                disallows.put(v, 0);
+            }
+            disallows.put(v, disallows.get(v) + 1);
+        }
+
+        public void disallow(List<Cell> affectedCells) {
+            for(Cell ac : affectedCells) {
+            	if(ac.value != null) {
+            		this.disallow(ac.value);
+            	}
+            }
+        }
+
+        public void allow(int v) {
+            int count = disallows.get(v);
+            if(count == 1) {
+                disallows.remove(v);
+            } else {
+                disallows.put(v, count - 1);
+            }
+        }
+
+        public void reset() {
+            this.step = null;
+            this.value = null;
+            this.lastUpdatedCells.clear();
+        }
+
+		@Override
+		public String toString() {
+			return "Cell [x=" + x + ", y=" + y + ", value=" + value
+					+ ", lastTry=" + lastTry + ", disallows=" + disallows + "]";
+		}
+    }
+
+    private static class CellComparator implements Comparator<Cell> {
+       
+        @Override
+        public int compare(Cell c1, Cell c2) {
+        	return c2.disallows.size() - c1.disallows.size();
+        }
+    }
+}
