185d9e440d0465ac13114adf9ae58045b99871bc
Yue Ni <niyue.com@gmail.com>
2013-06-28 16:24:00 +0800

    Solve two easy problems in hackerrank.

---
+++ src/main/java/com/niyue/coding/hackerrank/botsavesprincess/Solution.java
@@ -0,0 +1,53 @@
+package com.niyue.coding.hackerrank.botsavesprincess;
+
+import java.util.Scanner;
+// https://www.hackerrank.com/challenges/saveprincess
+public class Solution {
+   private static int[] m = new int[2];
+
+    /* Head ends here */
+    static void displayPathtoPrincess(int n, String [] grid) {
+        int[] p = princessCoordinates(n, grid);
+        String dx = p[1] < m[1] ? "LEFT" : "RIGHT";
+        String dy = p[0] < m[0] ? "UP" : "DOWN";
+        for(int x = 0; x < Math.abs(p[1] - m[1]); x++) {
+            System.out.println(dx);
+        }      
+        for(int y = 0; y < Math.abs(p[0] - m[0]); y++) {
+            System.out.println(dy);
+        }    
+    }
+    
+    private static int[] princessCoordinates(int n, String [] grid) {
+        int[] p = null;
+        int[][] corners = new int[][]{{0, 0}, {0, n - 1}, {n - 1, 0}, {n - 1, n - 1}};
+        for(int[] c : corners) {
+            if(hasPrincess(grid, c[0], c[1])) {
+               p = c; 
+            }
+        }
+        return p;
+    }
+    
+    private static boolean hasPrincess(String[] grid, int y, int x) {
+        return grid[y].charAt(x) == 'p';
+    }
+    
+    /* Tail starts here */
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n;
+        n = in.nextInt();
+        String grid[] = new String[n];
+        for(int i = 0; i < n; i++) {
+            grid[i] = in.next();
+            int mx = grid[i].indexOf('m');
+            if(mx != -1) {
+                m[0] = i;
+                m[1] = mx;
+            }
+        }
+
+        displayPathtoPrincess(n,grid);
+    }
+}
---
+++ src/main/java/com/niyue/coding/hackerrank/botsavesprincess2/Solution.java
@@ -0,0 +1,43 @@
+package com.niyue.coding.hackerrank.botsavesprincess2;
+
+import java.util.Scanner;
+
+// https://www.hackerrank.com/challenges/saveprincess2
+public class Solution {
+	private static int[] p = new int[2];
+
+	static void nextMove(int n, int x, int y, String[] board) {
+		String move = null;
+		if (p[0] != x) {
+			move = p[0] < x ? "UP" : "DOWN";
+		} else {
+			if (p[1] != y) {
+				move = p[1] < y ? "LEFT" : "RIGHT";
+			}
+		}
+		if (move != null) {
+			System.out.print(move);
+		}
+	}
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		int n, x, y;
+		n = in.nextInt();
+		x = in.nextInt();
+		y = in.nextInt();
+		in.useDelimiter("\n");
+		String board[] = new String[n];
+
+		for (int i = 0; i < n; i++) {
+			board[i] = in.next();
+			int py = board[i].indexOf('p');
+			if (py != -1) {
+				p[0] = i;
+				p[1] = py;
+			}
+		}
+
+		nextMove(n, x, y, board);
+	}
+}
