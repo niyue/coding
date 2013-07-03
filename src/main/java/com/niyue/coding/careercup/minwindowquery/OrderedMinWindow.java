bf353691b0b7bff9d8138f1f71dfc5c94808f704
Ni Yue <niyue.com@gmail.com>
2013-07-03 08:23:34 +0800

    Solve the minimum window query problem for an ordered query. The time complexity seems not O(n) as expected in some cases, some more optimization is needed.

--- src/main/java/com/niyue/coding/careercup/minwindowquery/OrderedMinWindow.java
+++ src/main/java/com/niyue/coding/careercup/minwindowquery/OrderedMinWindow.java
@@ -12,28 +12,35 @@ public class OrderedMinWindow {
 	public int[] minWindow(List<Integer> numbers, List<Integer> query) {
 		Map<Integer, List<Integer>> invertedIndex = invertedIndex(numbers);
 		int[] cursors = new int[query.size()];
-		minWindow(cursors, 0, -1, invertedIndex, query);
+		int firstInteger = query.get(0);
+		if(invertedIndex.containsKey(firstInteger)) {
+			int firstIntegerAppearances = invertedIndex.get(firstInteger).size();
+			while(cursors[0] < firstIntegerAppearances) {
+				minWindow(cursors, 0, -1, invertedIndex, query);
+				cursors[0]++;
+			}
+		}
 		return minWindow;
 	}
 	
 	private void minWindow(int[] cursors, int i, int startIndex, Map<Integer, List<Integer>> invertedIndex, List<Integer> query) {
 		int ci = cursors[i];
 		int number = query.get(i);
-		List<Integer> indexes = invertedIndex.get(number);
-		for(int j = ci; j < indexes.size(); j++) {
-			if(indexes.get(j) > startIndex) {
-				if(i == query.size() - 1) {
-					int start = invertedIndex.get(query.get(0)).get(cursors[0]);
-					int end = invertedIndex.get(query.get(i)).get(cursors[i]);
-					if(minWindow[0] == -1 || end - start < minWindow[1] - minWindow[0]) {
-						minWindow[0] = start;
-						minWindow[1] = end;
+		if(invertedIndex.containsKey(number)) {
+			List<Integer> indexes = invertedIndex.get(number);
+			for(int j = ci; j < indexes.size(); j++) {
+				if(indexes.get(j) > startIndex) {
+					if(i == query.size() - 1) {
+						int start = invertedIndex.get(query.get(0)).get(i == 0 ? cursors[0] : cursors[0] - 1);
+						int end = indexes.get(j);
+						if(minWindow[0] == -1 || end - start < minWindow[1] - minWindow[0]) {
+							minWindow[0] = start;
+							minWindow[1] = end;
+						}
+					} else {
+						cursors[i]++;
+						minWindow(cursors, i + 1, indexes.get(j), invertedIndex, query);
 					}
-					cursors[0]++;
-					minWindow(cursors, 0, -1, invertedIndex, query);
-				} else {
-					cursors[i]++;
-					minWindow(cursors, i + 1, indexes.get(j), invertedIndex, query);
 				}
 			}
 		}
