38adde247588b6df95e22a120b8617c9c05c421a
Ni Yue <niyue.com@gmail.com>
2013-07-07 11:36:49 +0800

    Implement quickselect selection algorithm.

---
+++ src/main/java/com/niyue/coding/careercup/quickselect/QuickSelect.java
@@ -0,0 +1,52 @@
+package com.niyue.coding.careercup.quickselect;
+
+// http://www.careercup.com/question?id=4356905
+public class QuickSelect {
+	public int select(int[] numbers, int k) {
+		int kth = quickSelect(numbers, k, 0, numbers.length - 1);
+		return kth;
+	}
+	
+	private int quickSelect(int[] numbers, int k, int start, int end) {
+		int kth = -1;
+		if(start <= end) {
+			int pivot = choosePivot(numbers, start, end);
+			int pivotPosition = partition(numbers, start, end, pivot);
+			if(pivotPosition - start == k) {
+				kth = numbers[pivotPosition];
+			} else if(pivotPosition - start < k) {
+				kth = quickSelect(numbers, k - pivotPosition - 1, pivotPosition + 1, end);
+			} else {
+				kth = quickSelect(numbers, k, start, pivotPosition - 1);
+			}
+		}
+		return kth;
+	}
+	
+	private int choosePivot(int[] numbers, int start, int end) {
+		return start;
+	}
+	
+	private int partition(int[] numbers, int start, int end, int pivot) {
+		int pivotValue = numbers[pivot];
+		swap(numbers, pivot, end);
+		int j = end - 1;
+		int i = start;
+		while(i <= j) {
+			if(numbers[i] > pivotValue) {
+				swap(numbers, i, j);
+				j--;
+			} else {
+				i++;
+			}
+		}
+		swap(numbers, i, end);
+		return i;
+	}
+	
+	private void swap(int[] numbers, int i, int j) {
+		int temp = numbers[i];
+		numbers[i] = numbers[j];
+		numbers[j] = temp;
+	}
+}
---
+++ src/test/java/com/niyue/coding/careercup/quickselect/QuickSelectTest.java
@@ -0,0 +1,44 @@
+package com.niyue.coding.careercup.quickselect;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+import org.junit.Test;
+
+public class QuickSelectTest {
+
+	@Test
+	public void testOneSelectFirst() {
+		QuickSelect qs = new QuickSelect();
+		int kth = qs.select(new int[] {1}, 0);
+		assertThat(kth, is(1));
+	}
+	
+	@Test
+	public void testTwoSelectFirst() {
+		QuickSelect qs = new QuickSelect();
+		int kth = qs.select(new int[] {1, 2}, 0);
+		assertThat(kth, is(1));
+	}
+	
+	@Test
+	public void testTwoSelectSecond() {
+		QuickSelect qs = new QuickSelect();
+		int kth = qs.select(new int[] {1, 2}, 1);
+		assertThat(kth, is(2));
+	}
+	
+	@Test
+	public void testThreeSelectSecond() {
+		QuickSelect qs = new QuickSelect();
+		int kth = qs.select(new int[] {3, 1, 2}, 1);
+		assertThat(kth, is(2));
+	}
+	
+	@Test
+	public void testComplex() {
+		QuickSelect qs = new QuickSelect();
+		int kth = qs.select(new int[] {3, 1, 2, 5, 5, 6}, 4);
+		assertThat(kth, is(5));
+	}
+}
