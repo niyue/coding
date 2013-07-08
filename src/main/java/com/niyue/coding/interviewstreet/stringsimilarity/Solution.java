c71d54906e79f3fbb35196aab93e083012fc290e
Yue Ni <niyue.com@gmail.com>
2013-07-08 17:31:50 +0800

    Add some comments for the solution for better understanding of the solution to finding a minimum cost of performing sorting using limited operations.

--- src/main/java/com/niyue/coding/interviewstreet/stringsimilarity/Solution.java
+++ src/main/java/com/niyue/coding/interviewstreet/stringsimilarity/Solution.java
@@ -1,14 +1,11 @@
 package com.niyue.coding.interviewstreet.stringsimilarity;
 
-import java.util.List;
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
-import java.util.Map;
-import java.util.HashMap;
 
 class Solution {
     private int T;
-    private Map<Integer, Map<Integer, Long>> similarities;
 
     public static void main(String[] args) throws java.lang.Exception {
         Solution sl = new Solution();
@@ -18,50 +15,12 @@ class Solution {
     public void solve() {
         List<String> strings = getInput();
         for (String string : strings) {
-            similarities = new HashMap<Integer, Map<Integer, Long>>();
-            System.out.println(similarity(string));
+        	System.out.println(sum(string));
         }
     }
 
-    private long similarity(String string) {
-        long count = 0;
-        for (int i = string.length() - 1; i >= 0; i--) {
-            long suffixSimilarity = similarity(string, 0, i);
-            count += suffixSimilarity;
-        }
-        return count;
-    }
-
-    private long similarity(String string, int startIndex, int suffixIndex) {
-        long similarity = 0;
-        if (hasSolution(startIndex, suffixIndex)) {
-            similarity = similarities.get(startIndex).get(suffixIndex);
-        } else {
-            if (string.length() > suffixIndex) {
-                if (string.charAt(startIndex) == string.charAt(suffixIndex)) {
-                    similarity = 1 + similarity(string, startIndex + 1,
-                            suffixIndex + 1);
-                }
-                setSolution(startIndex, suffixIndex, similarity);
-            }
-        }
-
-        return similarity;
-    }
-
-    private boolean hasSolution(int startIndex, int suffixIndex) {
-        boolean hasSolution = false;
-        if (similarities.containsKey(startIndex)) {
-            hasSolution = similarities.get(startIndex).containsKey(suffixIndex);
-        }
-        return hasSolution;
-    }
-
-    private void setSolution(int startIndex, int suffixIndex, long similarity) {
-        if (!similarities.containsKey(startIndex)) {
-            similarities.put(startIndex, new HashMap<Integer, Long>());
-        }
-        similarities.get(startIndex).put(suffixIndex, similarity);
+    private int sum(String s) {
+    	return 0;
     }
 
     private List<String> getInput() {
