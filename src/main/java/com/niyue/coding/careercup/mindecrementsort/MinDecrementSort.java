6476cb7d9a6aeb7639b1e91d0551e999bb5ae6fe
Yue Ni <niyue.com@gmail.com>
2013-07-08 17:35:08 +0800

    Add some comments for the solution for better understanding of the solution to finding a minimum cost of performing sorting using limited operations.

--- src/main/java/com/niyue/coding/careercup/mindecrementsort/MinDecrementSort.java
+++ src/main/java/com/niyue/coding/careercup/mindecrementsort/MinDecrementSort.java
@@ -15,21 +15,25 @@ public class MinDecrementSort {
 		return minCost;
 	}
 	
-	private int minCost(int start, int min) {
+	// minCost denotes the minimum cost for sorting using the allowed operations for numbers[0...current] when all these numbers are smaller than max
+	// max should be a number in the numbers array (except its initial value 0)
+	// if the current number is larger than the max, we could try to reduce it to any value between max...numbers[current] and pick up the minimum cost one
+	// if the current number is smaller than the max, we have to remove it entirely with the cost of numbers[current]
+	private int minCost(int current, int max) {
 		int cost = 0;
-		if(start < numbers.length) {
-			if(numbers[start] >= min) {
+		if(current < numbers.length) {
+			if(numbers[current] >= max) {
 				List<Integer> costs = new ArrayList<Integer>();
 				// according to the stackoverflow answer, a value can only be decreased to values in the array
 				for(int i = 0; i < numbers.length; i++) {
-					if(numbers[i] >= min && numbers[i] <= numbers[start]) {
-						int c = minCost(start + 1, numbers[i]) + numbers[start] - numbers[i];
+					if(numbers[i] >= max && numbers[i] <= numbers[current]) {
+						int c = minCost(current + 1, numbers[i]) + numbers[current] - numbers[i];
 						costs.add(c);
 					}
 				}
 				cost = Collections.min(costs);
 			} else {
-				cost = minCost(start + 1, min) + numbers[start];
+				cost = minCost(current + 1, max) + numbers[current];
 			}
 		}
 		return cost;
