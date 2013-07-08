776ed489ec90cae08ebc49e93aba9fea12c54e9b
Yue Ni <niyue.com@gmail.com>
2013-07-08 17:12:47 +0800

    Tweak the implementation a bit so that it could be turned into a top down DP solution.

--- src/main/java/com/niyue/coding/careercup/mindecrementsort/MinDecrementSort.java
+++ src/main/java/com/niyue/coding/careercup/mindecrementsort/MinDecrementSort.java
@@ -6,6 +6,7 @@ import java.util.List;
 
 // http://www.careercup.com/question?id=373367
 // http://stackoverflow.com/questions/8928061/convert-array-to-a-sorted-one-using-only-two-operations
+// TODO: could be turned to a top down O(n^2) DP solution using memoization
 public class MinDecrementSort {
 	private int[] numbers;
 	public int minCost(int[] numbers) {
@@ -19,9 +20,12 @@ public class MinDecrementSort {
 		if(start < numbers.length) {
 			if(numbers[start] >= min) {
 				List<Integer> costs = new ArrayList<Integer>();
-				for(int i = min; i <= numbers[start]; i++) {
-					int c = minCost(start + 1, i) + numbers[start] - i;
-					costs.add(c);
+				// according to the stackoverflow answer, a value can only be decreased to values in the array
+				for(int i = 0; i < numbers.length; i++) {
+					if(numbers[i] >= min && numbers[i] <= numbers[start]) {
+						int c = minCost(start + 1, numbers[i]) + numbers[start] - numbers[i];
+						costs.add(c);
+					}
 				}
 				cost = Collections.min(costs);
 			} else {
--- src/test/java/com/niyue/coding/careercup/mindecrementsort/MinDecrementSortTest.java
+++ src/test/java/com/niyue/coding/careercup/mindecrementsort/MinDecrementSortTest.java
@@ -22,6 +22,13 @@ public class MinDecrementSortTest {
 	}
 	
 	@Test
+	public void testTwoDescendinglySortedElements() {
+		MinDecrementSort sort = new MinDecrementSort();
+		int cost = sort.minCost(new int[]{2, 1});
+		assertThat(cost, is(1));
+	}
+	
+	@Test
 	public void testThreeElements() {
 		MinDecrementSort sort = new MinDecrementSort();
 		int cost = sort.minCost(new int[]{1, 3, 2});
@@ -43,6 +50,13 @@ public class MinDecrementSortTest {
 	}
 	
 	@Test
+	public void test312() {
+		MinDecrementSort sort = new MinDecrementSort();
+		int cost = sort.minCost(new int[]{3, 1, 2});
+		assertThat(cost, is(2));
+	}
+	
+	@Test
 	public void test1312() {
 		MinDecrementSort sort = new MinDecrementSort();
 		int cost = sort.minCost(new int[]{1, 3, 1, 2});
@@ -54,5 +68,12 @@ public class MinDecrementSortTest {
 		MinDecrementSort sort = new MinDecrementSort();
 		int cost = sort.minCost(new int[]{1, 3, 2, 2});
 		assertThat(cost, is(1));
+	}
+	
+	@Test
+	public void test21142443() {
+		MinDecrementSort sort = new MinDecrementSort();
+		int cost = sort.minCost(new int[]{2, 1, 1, 4, 2, 4, 4, 3});
+		assertThat(cost, is(5));
 	}
 }
