401ba6ec37fc99d9210a0a5c16689b554de4a27d
Ni Yue <niyue.com@gmail.com>
2013-07-17 08:35:54 +0800

    Using Integer's method to find the highest one bit for an integer.

--- src/main/java/com/niyue/coding/misc/mintwoexp/MinTwoExp.java
+++ src/main/java/com/niyue/coding/misc/mintwoexp/MinTwoExp.java
@@ -3,8 +3,21 @@ package com.niyue.coding.misc.mintwoexp;
 // http://www.mitbbs.com/article_t/JobHunting/32432949.html
 public class MinTwoExp {
 	public int search(int a, int b) {
+		return shiftOnceMethod(a, b);
+	}
+	
+	@SuppressWarnings("unused")
+	private int shiftLoopMethod(int a, int b) {
 		while(a > b) {
 			b <<= 1;
+		}
+		return b;
+	}
+	
+	// http://stackoverflow.com/questions/1815741/finding-highest-order-1-in-a-java-primitive
+	private int shiftOnceMethod(int a, int b) {
+		if(a > b) {
+			b = Integer.highestOneBit(a) << 1;
 		}
 		return b;
 	}
