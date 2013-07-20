dd9fed33684bb92532d5d102fb4e2ea37a464ed6
Ni Yue <niyue.com@gmail.com>
2013-07-20 10:48:42 +0800

    Replace the stupid O(lgn * lgn) solution with a simpler O(lgn) solution to find the closest node in BST.

--- src/main/java/com/niyue/coding/careercup/doublebst/DoubleBst.java
+++ src/main/java/com/niyue/coding/careercup/doublebst/DoubleBst.java
@@ -2,20 +2,13 @@ package com.niyue.coding.careercup.doublebst;
 
 // http://www.careercup.com/question?id=12697664
 // A BST, each of its node is of double value, find the closest node for some k
-// An O(lgn * lgn) solution to find the closest node in a BST with double value
+// An O(lgn) solution to find the closest node in a BST with double value
 public class DoubleBst {
 	public TreeNode findClosest(TreeNode node, double k) {
 		TreeNode closest = null;
 		if (node != null) {
-			if (node.value < k) {
-				TreeNode LeftClosest = findClosest(node.left, k);
-				TreeNode next = next(node.right);
-				closest = closest(LeftClosest, next, k);
-			} else if (node.value > k) {
-				TreeNode rightClosest = findClosest(node.right, k);
-				TreeNode prev = prev(node.left);
-				closest = closest(rightClosest, prev, k);
-			}
+			TreeNode branch = k < node.value ? node.left : node.right;
+			closest = findClosest(branch, k);
 			closest = closest(closest, node, k);
 		}
 		return closest;
@@ -23,27 +16,9 @@ public class DoubleBst {
 
 	private TreeNode closest(TreeNode n1, TreeNode n2, double k) {
 		if (n1 != null && n2 != null) {
-			return Math.abs(n1.value - k) > Math.abs(n2.value - k) ? n2 : n1;
+			return Math.abs(n1.value - k) < Math.abs(n2.value - k) ? n1 : n2;
 		} else {
 			return n1 != null ? n1 : n2;
 		}
-	}
-
-	private TreeNode prev(TreeNode node) {
-		TreeNode rightMost = null;
-		while (node != null) {
-			rightMost = node;
-			node = node.right;
-		}
-		return rightMost;
-	}
-
-	private TreeNode next(TreeNode node) {
-		TreeNode mostLeft = null;
-		while (node != null) {
-			mostLeft = node;
-			node = node.left;
-		}
-		return mostLeft;
 	}
 }
