4d891cfc42a9d5fb65226b00ec2929e287db3d49
Ni Yue <niyue.com@gmail.com>
2013-08-01 06:47:05 +0800

    Solve some edge cases for calculating the maximum points that can be seen within a given view port.

--- src/main/java/com/niyue/coding/misc/maxviewport/MaxViewPort.java
+++ src/main/java/com/niyue/coding/misc/maxviewport/MaxViewPort.java
@@ -4,20 +4,18 @@ import java.util.Arrays;
 
 // Given a set of points in coordinate system, and a view port, for example, 30 degrees, whose source is in origin point, 
 // figure out the maximum number of points can be seen in the view port
+// An O(n) solution using line sweeping
 public class MaxViewPort {
 	// the points in input are given with their polar system angular coordinate
 	// the radius is irrelevant in this problem and is ignored
 	public int count(double[] points, double viewPort) {
 		assert viewPort >= 0 && viewPort < 2 * Math.PI;
-		Arrays.sort(points);
-		double[] allPoints = Arrays.copyOf(points, points.length * 2);
-		for(int i = points.length; i < allPoints.length; i++) {
-			allPoints[i] = 2 * Math.PI + points[i - points.length];
-		}
+		points = sortedContinuousPoints(points);
+		
 		int max = 0;
-		int count = 1; 
-		for(int i = 0, j = -1; j < allPoints.length;) {
-			while(j + 1 < allPoints.length && allPoints[j + 1] - allPoints[i] < viewPort) {
+		int count = 0; 
+		for(int i = 0, j = 0; i < points.length && j < points.length;) {
+			while(j < points.length && points[j] - points[i] < viewPort) {
 				j++;
 				count++;
 			}
@@ -28,5 +26,14 @@ public class MaxViewPort {
 			count--;
 		}
 		return max;
+	}
+	
+	private double[] sortedContinuousPoints(double[] points) {
+		Arrays.sort(points);
+		double[] allPoints = Arrays.copyOf(points, points.length * 2);
+		for(int i = points.length; i < allPoints.length; i++) {
+			allPoints[i] = 2 * Math.PI + points[i - points.length];
+		}
+		return allPoints;
 	}
 }
--- src/test/java/com/niyue/coding/misc/maxviewport/MaxViewPortTest.java
+++ src/test/java/com/niyue/coding/misc/maxviewport/MaxViewPortTest.java
@@ -13,5 +13,40 @@ public class MaxViewPortTest {
 		int max = viewPort.count(new double[] {0.5}, 0.1);
 		assertThat(max, is(1));
 	}
+	
+	@Test
+	public void testTwoPoints() {
+		MaxViewPort viewPort = new MaxViewPort();
+		int max = viewPort.count(new double[] {0.5, 0.6}, 0.1);
+		assertThat(max, is(2));
+	}
+	
+	@Test
+	public void testThreePoints() {
+		MaxViewPort viewPort = new MaxViewPort();
+		int max = viewPort.count(new double[] {0.5, 0.55, 0.6}, 0.1);
+		assertThat(max, is(3));
+	}
+	
+	@Test
+	public void testFourPoints() {
+		MaxViewPort viewPort = new MaxViewPort();
+		int max = viewPort.count(new double[] {0.1, 0.5, 0.55, 0.6, 1.1}, 0.1);
+		assertThat(max, is(3));
+	}
+	
+	@Test
+	public void testMultiplePoints() {
+		MaxViewPort viewPort = new MaxViewPort();
+		int max = viewPort.count(new double[] {0.1, 0.5, 0.55, 0.6, 1.1, 1.9, 2.0, 2.01, 2.02, 2.03, 2.04, 3}, 0.1);
+		assertThat(max, is(5));
+	}
+	
+	@Test
+	public void testOverlappedPoints() {
+		MaxViewPort viewPort = new MaxViewPort();
+		int max = viewPort.count(new double[] {0.01, 0.02, 0.5, 0.55, 0.6, Math.PI * 2 - 0.01, Math.PI * 2 - 0.02}, 0.1);
+		assertThat(max, is(4));
+	}
 
 }
