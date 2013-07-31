3240a5e0839579e14faf2fe664cc4c424181776e
Yue Ni <niyue.com@gmail.com>
2013-07-31 11:07:27 +0800

    Simplify the in order iterative traversal for a binary tree, without using a parent stack, a temp variable is enough to implement the in order iterator.

--- src/main/java/com/niyue/coding/careercup/bsttwosum/InorderIterator.java
+++ src/main/java/com/niyue/coding/careercup/bsttwosum/InorderIterator.java
@@ -1,33 +1,23 @@
 package com.niyue.coding.careercup.bsttwosum;
 
+import java.util.ArrayDeque;
 import java.util.Deque;
-import java.util.HashMap;
 import java.util.Iterator;
-import java.util.LinkedList;
-import java.util.Map;
 import java.util.NoSuchElementException;
 
 import com.niyue.coding.leetcode.common.TreeNode;
 
 class InorderIterator implements Iterator<TreeNode> {
-    private Deque<TreeNode> stack = new LinkedList<TreeNode>();
-    private Map<TreeNode, TreeNode> parents = new HashMap<TreeNode, TreeNode>();
+    private Deque<TreeNode> stack = new ArrayDeque<TreeNode>();
+    private TreeNode current = null;
     
     public InorderIterator(TreeNode root) {
-        parents.put(root, null);
-        TreeNode current = root;
-        TreeNode parent = null;
-		while(current != null) {
-			stack.addFirst(current);
-			parents.put(current, parent);
-			parent = current;
-			current = current.left;
-		}
+    	current = root;
     }
     
 	@Override
 	public boolean hasNext() {
-		return !stack.isEmpty();
+		return !stack.isEmpty() || current != null;
 	}
 
 	@Override
@@ -40,31 +30,13 @@ class InorderIterator implements Iterator<TreeNode> {
 	}
 	
 	private TreeNode nextNode() {
-		TreeNode next = null;
-		if(!stack.isEmpty()) {
-			TreeNode current = stack.removeFirst();
-			TreeNode parent = parents.remove(current);
-			
-			if(current.right != null) {
-				TreeNode nextNode = current.right;
-				TreeNode nextParent = null;
-				while(nextNode != null) {
-					stack.addFirst(nextNode);
-					nextParent = nextNode;
-					nextNode = nextNode.left;
-					parents.put(nextNode, nextParent);
-				}
-			} else {
-				TreeNode childNode = current;
-				TreeNode parentNode = parent;
-				while(parentNode != null && childNode == parentNode.right) {
-					childNode = parentNode;
-					stack.removeFirst();
-					parentNode = parents.remove(parentNode);
-				}
-			}
-			next = current;
+		while(current != null) {
+			stack.addFirst(current);
+			current = current.left;
 		}
+		current = stack.removeFirst();
+		TreeNode next = current;
+		current = current.right;
 		return next;
 	}
 
--- src/main/java/com/niyue/coding/careercup/bsttwosum/ReverseInorderIterator.java
+++ src/main/java/com/niyue/coding/careercup/bsttwosum/ReverseInorderIterator.java
@@ -1,33 +1,23 @@
 package com.niyue.coding.careercup.bsttwosum;
 
+import java.util.ArrayDeque;
 import java.util.Deque;
-import java.util.HashMap;
 import java.util.Iterator;
-import java.util.LinkedList;
-import java.util.Map;
 import java.util.NoSuchElementException;
 
 import com.niyue.coding.leetcode.common.TreeNode;
 
 class ReverseInorderIterator implements Iterator<TreeNode> {
-	private Deque<TreeNode> stack = new LinkedList<TreeNode>();
-    private Map<TreeNode, TreeNode> parents = new HashMap<TreeNode, TreeNode>();
+	private Deque<TreeNode> stack = new ArrayDeque<TreeNode>();
+	private TreeNode current = null;
     
     public ReverseInorderIterator(TreeNode root) {
-        parents.put(root, null);
-        TreeNode current = root;
-        TreeNode parent = null;
-		while(current != null) {
-			stack.addFirst(current);
-			parents.put(current, parent);
-			parent = current;
-			current = current.right;
-		}
+        current = root;
     }
     
 	@Override
 	public boolean hasNext() {
-		return !stack.isEmpty();
+		return !stack.isEmpty() || current != null;
 	}
 
 	@Override
@@ -40,31 +30,13 @@ class ReverseInorderIterator implements Iterator<TreeNode> {
 	}
 	
 	private TreeNode nextNode() {
-		TreeNode next = null;
-		if(!stack.isEmpty()) {
-			TreeNode current = stack.removeFirst();
-			TreeNode parent = parents.remove(current);
-			
-			if(current.left != null) {
-				TreeNode nextNode = current.left;
-				TreeNode nextParent = null;
-				while(nextNode != null) {
-					stack.addFirst(nextNode);
-					nextParent = nextNode;
-					nextNode = nextNode.right;
-					parents.put(nextNode, nextParent);
-				}
-			} else {
-				TreeNode childNode = current;
-				TreeNode parentNode = parent;
-				while(parentNode != null && childNode == parentNode.left) {
-					childNode = parentNode;
-					stack.removeFirst();
-					parentNode = parents.remove(parentNode);
-				}
-			}
-			next = current;
+		while(current != null) {
+			stack.addFirst(current);
+			current = current.right;
 		}
+		current = stack.removeFirst();
+		TreeNode next = current;
+		current = current.left;
 		return next;
 	}
 
