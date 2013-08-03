24ca5aa2afb3fe289e36af5e48469220e726c284
Ni Yue <niyue.com@gmail.com>
2013-08-03 13:35:54 +0800

    Use a map for quick look up instead of doing linear search for index.

--- src/main/java/com/niyue/coding/leetcode/constructbinaryinorderpostorder/Solution.java
+++ src/main/java/com/niyue/coding/leetcode/constructbinaryinorderpostorder/Solution.java
@@ -1,10 +1,17 @@
 package com.niyue.coding.leetcode.constructbinaryinorderpostorder;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import com.niyue.coding.leetcode.common.TreeNode;
 
 // http://leetcode.com/onlinejudge#question_106
+// The original algorithm uses linear search in the array, which will lead to an O(n^2) algorithm
+// the current algorithm uses a Map to store all the indexes for each number, and uses the map for quick look up 
 public class Solution {
+	private Map<Integer, Integer> indexMap;
     public TreeNode buildTree(int[] inorder, int[] postorder) {
+    	indexMap = indexMap(inorder);
     	return build(inorder, 0, inorder.length - 1, postorder, 0, postorder.length - 1);        
     }
 
@@ -13,7 +20,7 @@ public class Solution {
     	if(s1 <= e1) {
     		int parentVal = postorder[e2];
     		parent = new TreeNode(parentVal);
-    		int parentIndex = search(inorder, s1, e1 + 1, parentVal);
+    		int parentIndex = search(indexMap, parentVal);
     		TreeNode left = build(inorder, s1, parentIndex - 1, postorder, s2, s2 + parentIndex - s1 - 1);
     		TreeNode right = build(inorder, parentIndex + 1, e1, postorder, s2 + parentIndex - s1, e2 - 1);
     		parent.left = left;
@@ -21,13 +28,26 @@ public class Solution {
     	}
     	return parent;
     }
-
-    private int search(int[] inorder, int start, int end, int target) {
+    
+    private Map<Integer, Integer> indexMap(int[] inorder) {
+    	Map<Integer, Integer> map = new HashMap<Integer, Integer>();
+    	for(int i = 0; i < inorder.length; i ++) {
+    		map.put(inorder[i], i);
+    	}
+    	return map;
+    }
+    
+    @SuppressWarnings("unused")
+	private int search(int[] inorder, int start, int end, int target) {
     	for(int i = start; i < end; i++) {
     		if(inorder[i] == target) {
     			return i;
     		}
     	}
     	return -1;
+    }
+    
+    private int search(Map<Integer, Integer> indexMap, int target) {
+    	return indexMap.get(target);
     }
 }
