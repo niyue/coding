004d0639ad37670a1d5a96dd17dd1005245f68af
Yue Ni <niyue.com@gmail.com>
2013-08-18 18:10:18 +0800

    Make the implementation slightly more clear, and add some comments to explain the solution.

--- src/main/java/com/niyue/coding/leetcode/substringconcatenation/Solution.java
+++ src/main/java/com/niyue/coding/leetcode/substringconcatenation/Solution.java
@@ -7,6 +7,16 @@ import java.util.List;
 import java.util.Map;
 
 // http://leetcode.com/onlinejudge#question_30
+/*
+ * 1) Count number of times for each word, O(L * wordLength)
+ * 2) Find all indexes for each word in S, O(S * wordLength)
+ * 3) Find all continuous equally spaced words, O(S * wordLength)
+ * 		For each set of continuous equally spaced words, find if there is a set of words matching L given
+ * 			Keep a rolling window [start, end]
+ * 				if [start, end] matches word count for L, add it to solution
+ * 				else if a word appears more times than in L, slide the window to move the first appearance of this word out of the window
+ * Time complexity: O((L + S) * wordLength), where wordLength is a small constant typically
+ */
 public class Solution {
     public ArrayList<Integer> findSubstring(String S, String[] L) {
     	char[] chars = S.toCharArray();
@@ -23,7 +33,7 @@ public class Solution {
 					equallySpacedWords.add(key);
 					indexes.remove(key);
 				}
-				solutions.addAll(findSubstring(equallySpacedWords, wordsInS, wordLength, wordCount, L.length));
+				solutions.addAll(findSubstring(equallySpacedWords, wordsInS, wordCount, L.length));
 			}
 		}   
 		return solutions;     
@@ -42,12 +52,11 @@ public class Solution {
 
     private List<Integer> findSubstring(List<Integer> words, 
     		final Map<Integer, String> indexes, 
-    		final int wordLength, 
     		final Map<String, Integer> wordCount, 
     		int wordSetSize) {
     	List<Integer> solutions = new ArrayList<Integer>();
     	if(words.size() >= wordSetSize) {
-    		Map<String, List<Integer>> positions = new HashMap<String, List<Integer>>();
+    		Map<String, LinkedList<Integer>> positions = new HashMap<String, LinkedList<Integer>>();
     		int start = 0;
     		int wordsSize = 0;
     		for(int i = 0; i < words.size(); i++) {
@@ -55,13 +64,10 @@ public class Solution {
     			if(!positions.containsKey(word)) {
     				positions.put(word, new LinkedList<Integer>());
     			} else if(positions.get(word).size() == wordCount.get(word)) {
-    				int previousPosition = positions.get(word).get(0);
-    				for(int j = start; j <= previousPosition; j++) {
-    					String w = indexes.get(words.get(j));
-    					positions.get(w).remove(0);
-    					wordsSize--;
-    				}
-    				start = previousPosition + 1;
+    				int nextStart = positions.get(word).getFirst() + 1;
+    				wordsSize -= nextStart - start;
+    				slideWindowStart(positions, indexes, words, start, nextStart);
+    				start = nextStart;
     			}
     			positions.get(word).add(i);
     			wordsSize++;
@@ -71,6 +77,13 @@ public class Solution {
     		}
     	}
     	return solutions;
+    }
+    
+    private void slideWindowStart(Map<String, LinkedList<Integer>> positions, Map<Integer, String> indexes, List<Integer> words, int start, int nextStart) {
+		for(int j = start; j < nextStart; j++) {
+			String w = indexes.get(words.get(j));
+			positions.get(w).removeFirst();
+		}
     }
 
     private Map<String, Integer> wordCount(String[] L) {
