9f1d355d858851514ba7866f9726f987b12b7cfc
Ni Yue <niyue.com@gmail.com>
2013-09-02 20:08:25 +0800

    Add a function signature for coin change problem, and will resolve it later on.

---
+++ src/main/java/com/niyue/coding/careercup/coinchange/CoinChange.java
@@ -0,0 +1,15 @@
+package com.niyue.coding.careercup.coinchange;
+
+/*
+ * http://www.careercup.com/question?id=11585834
+ * You have coins with different denominations,
+ *  and you have limited number of each denominations (some maybe zero).
+ *  How will you determine if you can supply the given change.
+ */
+public class CoinChange {
+	public boolean isPossible(int[] denominations, int[] numbers, int change) {
+		assert denominations.length == numbers.length && change >= 0;
+		
+		return false;
+	}
+}
