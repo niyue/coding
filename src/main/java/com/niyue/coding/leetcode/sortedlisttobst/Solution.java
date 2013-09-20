4bfec2482fa26f334aac8fc90ca7e057de097cdd
Yue Ni <niyue.com@gmail.com>
2013-09-20 17:35:16 +0800

    Improve the implementation slightly when coding against the new online judge.

--- src/main/java/com/niyue/coding/leetcode/sortedlisttobst/Solution.java
+++ src/main/java/com/niyue/coding/leetcode/sortedlisttobst/Solution.java
@@ -3,40 +3,38 @@ package com.niyue.coding.leetcode.sortedlisttobst;
 import com.niyue.coding.leetcode.common.ListNode;
 import com.niyue.coding.leetcode.common.TreeNode;
 
+// http://oj.leetcode.com/problems/convert-sorted-list-to-binary-search-tree/
 // http://leetcode.com/onlinejudge#question_109
 // O(n) solution to convert sorted linked list to BST
 public class Solution {
+    private ListNode cursor = null;
     public TreeNode sortedListToBST(ListNode head) {
         int length = getLength(head);
-        
-        return (TreeNode) convert(head, 0, length - 1)[0];
+        cursor = head;
+        return listToBST(0, length - 1);
+    }
+    
+    private TreeNode listToBST(int start, int end) {
+        TreeNode parent = null;
+        if(start <= end) {
+            int mid = start + (end - start) / 2;
+            TreeNode left = listToBST(start, mid - 1);
+            parent = new TreeNode(cursor.val);
+            cursor = cursor.next; // NOTE: use a member variable so that this cursor advancement will take effect 
+            TreeNode right = listToBST(mid + 1, end);
+            parent.left = left;
+            parent.right = right;    
+        }
+        return parent;
     }
     
     private int getLength(ListNode head) {
         int length = 0;
-        for(ListNode cursor = head; cursor != null; cursor = cursor.next) {
+        ListNode current = head;
+        while(current != null) {
+            current = current.next;
             length++;
         }
         return length;
-    }
-    
-    private Object[] convert(ListNode node, int start, int end) {
-        TreeNode root = null;
-        if(start <= end) {
-            int mid = start + (end - start) / 2;
-            Object[] treeNodeAndListNode = convert(node, start, mid - 1);
-            TreeNode left = (TreeNode) treeNodeAndListNode[0];
-            node = (ListNode) treeNodeAndListNode[1];
-            TreeNode parent = new TreeNode(node.val);
-            node = node.next;
-            treeNodeAndListNode = convert(node, mid + 1, end);
-            TreeNode right = (TreeNode) treeNodeAndListNode[0];
-            node = (ListNode) treeNodeAndListNode[1];
-            parent.left = left;
-            parent.right = right;    
-            root = parent;
-        }
-        
-        return new Object[] {root, node};
     }
 }
