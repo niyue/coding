1396dc16781ed26a2f5c7e2d153e83877f2ff869
Yue Ni <niyue.com@gmail.com>
2013-09-21 09:34:30 +0800

    Reimplemented setting the next sibling node for the new online judge and remove some unnecessary checking for leaf nodes.

--- src/main/java/com/niyue/coding/leetcode/nextrightpointerii/Solution.java
+++ src/main/java/com/niyue/coding/leetcode/nextrightpointerii/Solution.java
@@ -1,50 +1,40 @@
 package com.niyue.coding.leetcode.nextrightpointerii;
 
-
+// http://oj.leetcode.com/problems/populating-next-right-pointers-in-each-node-ii/
 // http://leetcode.com/onlinejudge#question_117
 public class Solution {
-    public void connect(TreeLinkNode node) {
-        if(node != null && !isLeaf(node)) {
-        	TreeLinkNode siblingChild = siblingChild(node);
-        	if(node.right != null) {
-                node.right.next = siblingChild; 
+    public void connect(TreeLinkNode root) {
+        if(root != null) {
+            TreeLinkNode next = nextSiblingWithChild(root);
+            TreeLinkNode nextChild = nextChild(next);
+            if(root.right != null) {
+                root.right.next = nextChild;
             }
-        	
-            if(node.left != null) {
-            	node.left.next = node.right != null ? node.right : siblingChild;
+            if(root.left != null) {
+                root.left.next = root.right != null ? root.right : nextChild;
             }
-            
             // NOTE: process right sub tree before left sub tree
-            connect(node.right);    
-        
-            connect(node.left);    
-        }
+            connect(root.right);
+            connect(root.left);
+        }        
     }
     
-    private TreeLinkNode siblingChild(TreeLinkNode node) {
+    private TreeLinkNode nextSiblingWithChild(TreeLinkNode node) {
         TreeLinkNode next = node.next;
-        while(!isNodeWithAtLeastOneChild(next)) {
+        while(next != null && next.left == null && next.right == null) {
             next = next.next;
         }
-        TreeLinkNode sibling = null;
-        if(next != null) {
-        	sibling = next.left != null ? next.left : next.right;
-        }
-        return sibling;
+        return next;
     }
-    
-    private boolean isNodeWithAtLeastOneChild(TreeLinkNode node) {
-    	return node == null || node.left != null || node.right != null;
-    }
-    
-    private boolean isLeaf(TreeLinkNode node) {
-        return node.left == null && node.right == null;
+
+    private TreeLinkNode nextChild(TreeLinkNode node) {
+    	return node == null ? null : node.left != null ? node.left : node.right;
     }
     
     @SuppressWarnings("unused")
     private static class TreeLinkNode {
     	int val;
     	TreeLinkNode left, right, next;
-		TreeLinkNode(int x) { val = x; }
-	}
+    	TreeLinkNode(int x) { val = x; }
+    }
 }
