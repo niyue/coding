05ec72749fbb2064d9dd38565ffd8ca42ff77a5f
Yue Ni <niyue.com@gmail.com>
2013-09-21 09:06:14 +0800

    Implemented the reversed level order traversal of a tree, and found ArrayDeque will throw NPE for offer(null) and cannot be replace LinkedList in this case.

--- src/main/java/com/niyue/coding/leetcode/bottomupleveltraversal/Solution.java
+++ src/main/java/com/niyue/coding/leetcode/bottomupleveltraversal/Solution.java
@@ -7,16 +7,17 @@ import java.util.Queue;
 
 import com.niyue.coding.leetcode.common.TreeNode;
 
+// http://oj.leetcode.com/problems/binary-tree-level-order-traversal-ii/
 // http://leetcode.com/onlinejudge#question_107
 // BFS to traverse the tree and reverse the entire result at the end
 public class Solution {
     public ArrayList<ArrayList<Integer>> levelOrderBottom(TreeNode root) {
         ArrayList<ArrayList<Integer>> levels = new ArrayList<ArrayList<Integer>>();
         if(root != null) {
+        	// NOTE: ArrayDeque cannot be used in this case because queue.offer(null) will throw NPE for ArrayDeque
         	Queue<TreeNode> queue = new LinkedList<TreeNode>();
-        	queue.offer(root);
         	queue.offer(null);
-        	levels.add(new ArrayList<Integer>());
+        	queue.offer(root);
         	while(!(queue.size() == 1 && queue.peek() == null)) {
         		TreeNode node = queue.poll();
         		if(node == null) {
