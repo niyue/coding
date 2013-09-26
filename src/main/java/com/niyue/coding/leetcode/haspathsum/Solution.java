954df22f6012991be7764e0731467239af909bcd
Ni Yue <niyue.com@gmail.com>
2013-09-26 21:54:14 +0800

    Simplify hasPathSum implementation by unifying many edge cases.

--- src/main/java/com/niyue/coding/leetcode/haspathsum/Solution.java
+++ src/main/java/com/niyue/coding/leetcode/haspathsum/Solution.java
@@ -2,22 +2,18 @@ package com.niyue.coding.leetcode.haspathsum;
 
 import com.niyue.coding.leetcode.common.TreeNode;
 
-// http://leetcode.com/onlinejudge#question_112
+/*
+ *  http://leetcode.com/onlinejudge#question_112
+ *  http://oj.leetcode.com/problems/path-sum/
+ */
+
 public class Solution {
-    public boolean hasPathSum(TreeNode node, int sum) {
-        boolean hasPathSum = false;
-        if(node != null) {
-        	if(node.left != null && node.right == null) {
-        		hasPathSum = hasPathSum(node.left, sum - node.val);                
-        	} else if(node.left == null && node.right != null) {
-        		hasPathSum = hasPathSum(node.right, sum - node.val);
-        	} else if(node.left != null && node.right != null) {
-        		hasPathSum = hasPathSum(node.left, sum - node.val) ||
-        				hasPathSum(node.right, sum - node.val);
-        	} else {
-        		hasPathSum = node.val == sum;   
-        	}
+    public boolean hasPathSum(TreeNode root, int sum) {
+        if(root == null) {
+            return sum == 0;
+        } else {
+            return hasPathSum(root.left, sum - root.val) || 
+            	   hasPathSum(root.right, sum - root.val);
         }
-        return hasPathSum;
     }
 }
