86623afb13550baeba2421794c973ee66b3a7ae9
Ni Yue <niyue.com@gmail.com>
2013-09-27 22:25:59 +0800

    Simplify the binary search logic to incorporate 0 and 1 in it.

--- src/main/java/com/niyue/coding/leetcode/sqrt/Solution.java
+++ src/main/java/com/niyue/coding/leetcode/sqrt/Solution.java
@@ -4,23 +4,18 @@ package com.niyue.coding.leetcode.sqrt;
 // Use binary search to get the square root of a number
 public class Solution {
     public int sqrt(int x) {
-        if(x == 0 || x == 1) {
-            return x;   
-        }
-        int sqrt = binarySearch(1, x / 2, x);
-        return sqrt;
-    }
-    
-    private int binarySearch(int start, int end, long x) {
+        int start = 0;
+        int end = x;
+        int mid = 0;
         while(start <= end) {
-            int mid = start + (end - start) / 2;
-            long square = (long) mid * (long) mid;
-            if(square == x) {
-                return mid;
-            } else if(square < x) {
+            mid = start + (end - start) / 2;
+            long s = (long) mid * (long) mid;
+            if(s < x) {
                 start = mid + 1;
+            } else if(s == x) {
+                return mid;
             } else {
-               end = mid - 1;
+                end = mid - 1;
             }
         }
         return start - 1;
