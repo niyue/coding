7e5c7f7046495005449f806e7f50cbe06a6de38f
Ni Yue <niyue.com@gmail.com>
2013-09-28 13:03:01 +0800

    Add one to a multi digit number.

--- src/main/java/com/niyue/coding/leetcode/plusone/Solution.java
+++ src/main/java/com/niyue/coding/leetcode/plusone/Solution.java
@@ -1,17 +1,21 @@
 package com.niyue.coding.leetcode.plusone;
 
 
-// http://leetcode.com/onlinejudge#question_66
+/*
+ *  http://leetcode.com/onlinejudge#question_66
+ *  http://oj.leetcode.com/problems/plus-one/
+ */
+
 public class Solution {
     public int[] plusOne(int[] digits) {
         int carry = 1;
         for(int i = digits.length - 1; i >= 0; i--) {
             digits[i] = digits[i] + carry;
             if(digits[i] == 10) {
-                carry = 1;
                 digits[i] = 0;
             } else {
                 carry = 0;
+                break;
             }
         }        
         
