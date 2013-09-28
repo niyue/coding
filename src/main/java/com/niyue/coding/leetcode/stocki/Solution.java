722a757ae8483d6231ef27c412fd3c7fa0ecf634
Ni Yue <niyue.com@gmail.com>
2013-09-28 17:16:51 +0800

    Simplify the implementation a bit by unifying one corner case.

--- src/main/java/com/niyue/coding/leetcode/stocki/Solution.java
+++ src/main/java/com/niyue/coding/leetcode/stocki/Solution.java
@@ -5,20 +5,16 @@ package com.niyue.coding.leetcode.stocki;
 // If you were only permitted to complete at most one transaction (ie, buy one and sell one share of the stock), design an algorithm to find the maximum profit.
 public class Solution {
     public int maxProfit(int[] prices) {
+        int min = Integer.MAX_VALUE;
         int maxProfit = 0;
-        if(prices.length >= 2) {
-            int min = prices[0];
-            for(int i = 1;i < prices.length;i++) {
-                int profit = prices[i] - min;
-                if(profit > maxProfit) {
-                    maxProfit = profit;
-                }
-                if(prices[i] < min) {
-                    min = prices[i];
-                }
-            }    
+        for(int i = 0; i < prices.length; i++) {
+            if(prices[i] - min > maxProfit) {
+                maxProfit = prices[i] - min;
+            }
+            if(prices[i] < min) {
+                min = prices[i];
+            }
         }
-        
         return maxProfit;
     }
 }
