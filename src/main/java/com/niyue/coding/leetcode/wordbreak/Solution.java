0b0e649c2a329e40f6e741268ccfcc36db05bfed
Yue Ni <niyue.com@gmail.com>
2013-10-06 12:38:09 +0800

    Improve the DP to be O(n^3) from O(n^4) by removing a unnecessary loop.

--- src/main/java/com/niyue/coding/leetcode/wordbreak/Solution.java
+++ src/main/java/com/niyue/coding/leetcode/wordbreak/Solution.java
@@ -3,26 +3,24 @@ package com.niyue.coding.leetcode.wordbreak;
 import java.util.Set;
 
 /*
- * seg(i, j) denotes if s[i, j] is segmentable
- * seg(i, j) = for any k between i..j, dict.contains(s[i, k]) && seg[k + 1, j]
- * O(n^4) solution using DP
+ * seg(i) denotes if s[i, end] is segmentable
+ * seg(i) = for any k between i..end, dict.contains(s[i, k]) && seg(k + 1)
+ * O(n^3) solution using DP
  */
 public class Solution {
     public boolean wordBreak(String s, Set<String> dict) {
-        boolean[][] segmentable = new boolean[s.length() + 1][s.length() + 1];        
-        segmentable[s.length()][s.length()] = true;
+        boolean[] segmentable = new boolean[s.length() + 1];        
+        segmentable[s.length()] = true;
         
         for(int i = s.length() - 1; i >= 0; i--) {
-            for(int j = s.length(); j >= i; j--) {
-                for(int k = i; k < j; k++) {
-                    String word = s.substring(i, k + 1);    
-                    if(segmentable[k + 1][j] && dict.contains(word)) {
-                        segmentable[i][j] = true;
-                        break; 
-                    }    
-                }
+            for(int k = i; k < s.length(); k++) {
+                String word = s.substring(i, k + 1);    
+                if(segmentable[k + 1] && dict.contains(word)) {
+                    segmentable[i] = true;
+                    break; 
+                }    
             }
         }
-        return segmentable[0][s.length()];
+        return segmentable[0];
     }
 }
