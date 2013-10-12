0c1a5de05b56488499ed3b58bdbe06eeff33cdfd
Yue Ni <niyue.com@gmail.com>
2013-10-12 14:20:48 +0800

    Slightly simplified implementation.

--- src/main/java/com/niyue/coding/leetcode/sametree/Solution.java
+++ src/main/java/com/niyue/coding/leetcode/sametree/Solution.java
@@ -2,19 +2,17 @@ package com.niyue.coding.leetcode.sametree;
 
 import com.niyue.coding.leetcode.common.TreeNode;
 
-// http://leetcode.com/onlinejudge#question_100
-// recursively check if left and right sub tree are the same
+/*
+ * http://oj.leetcode.com/problems/same-tree/
+ * http://leetcode.com/onlinejudge#question_100
+ * recursively check if left and right sub tree are the same
+ */
 public class Solution {
     public boolean isSameTree(TreeNode p, TreeNode q) {
-        boolean same = false;
-        if(p == null && q == null) {
-            same = true;
-        } else if(p != null && q != null) {
-            same = 
-                p.val == q.val &&
-                isSameTree(p.left, q.left) &&
-                isSameTree(p.right, q.right);
+        boolean isSame = p == null && q == null;
+        if(!isSame && p != null && q != null) {
+            isSame = p.val == q.val && isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
         }
-        return same;
+        return isSame;
     }
 }
