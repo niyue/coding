cee5c499aef834f58790166b4462ce4f0a0d5a87
Yue Ni <niyue.com@gmail.com>
2013-10-22 12:16:15 +0800

    Simplify the old solution for reversing an integer.

--- src/main/java/com/niyue/coding/leetcode/reverseinteger/Solution.java
+++ src/main/java/com/niyue/coding/leetcode/reverseinteger/Solution.java
@@ -3,14 +3,9 @@ package com.niyue.coding.leetcode.reverseinteger;
 // http://leetcode.com/onlinejudge#question_7
 public class Solution {
     public int reverse(int x) {
-        boolean positive = true;
-        if(x < 0) {
-            positive = false; 
-            x = -1 * x;
-        }
-        String integerString = Integer.valueOf(x).toString();
+        String integerString = Integer.valueOf(Math.abs(x)).toString();
         String reversed = new StringBuilder(integerString).reverse().toString();
         int rx = Integer.valueOf(reversed);
-        return positive ? rx : rx * -1;
+        return x > 0 ? rx : -rx;
     }
 }
