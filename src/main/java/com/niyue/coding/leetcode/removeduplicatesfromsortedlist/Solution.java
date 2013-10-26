63043bf5affca98bde4dd0142c3563177abadc45
Yue Ni <niyue.com@gmail.com>
2013-10-26 08:51:35 +0800

    Simplify the implementation for edge case.

--- src/main/java/com/niyue/coding/leetcode/removeduplicatesfromsortedlist/Solution.java
+++ src/main/java/com/niyue/coding/leetcode/removeduplicatesfromsortedlist/Solution.java
@@ -2,20 +2,21 @@ package com.niyue.coding.leetcode.removeduplicatesfromsortedlist;
 
 import com.niyue.coding.leetcode.common.ListNode;
 
-// http://leetcode.com/onlinejudge#question_83
+/* 
+ * http://oj.leetcode.com/problems/remove-duplicates-from-sorted-list/
+ * http://leetcode.com/onlinejudge#question_83
+ */
 public class Solution {
     public ListNode deleteDuplicates(ListNode head) {
-        if(head != null) {
-            ListNode prev = head;
-            ListNode current = head.next;
-            while(current != null) {
-                if(current.val == prev.val) {
-                    prev.next = current.next;
-                } else {
-                    prev = current;
-                }
-                current = current.next;
+        ListNode current = head;
+        ListNode prev = null;
+        while(current != null) {
+            if(prev != null && current.val == prev.val) {
+                prev.next = current.next;
+            } else {
+                prev = current;
             }
+            current = current.next;
         }
         return head;
     }
