3d68b850652d1c20a3cdc900ffad6f30acdfc4d7
Yue Ni <niyue.com@gmail.com>
2013-10-26 12:26:38 +0800

    Improve the logic slightly when merging two sorted lists.

--- src/main/java/com/niyue/coding/leetcode/mergetwosortedlist/Solution.java
+++ src/main/java/com/niyue/coding/leetcode/mergetwosortedlist/Solution.java
@@ -5,27 +5,25 @@ import com.niyue.coding.leetcode.common.ListNode;
 // http://leetcode.com/onlinejudge#question_21
 public class Solution {
     public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
-        if(l2 == null) {
-            return l1;
-        } else if(l1 == null) {
+        if(l1 == null) {
             return l2;
+        } else if(l2 == null) {
+            return l1;
         } else {
             ListNode head = l1.val <= l2.val ? l1 : l2;
             ListNode p1 = l1;
             ListNode p2 = l2;
             ListNode prev = null;
             while(p1 != null && p2 != null) {
-                ListNode current = p1.val <= p2.val ? p1 : p2;
+                ListNode small = p1.val <= p2.val ? p1 : p2;
                 if(prev != null) {
-                    prev.next = current;
+                    prev.next = small;
                 }
-                prev = current;
-                current = current.next;
-
-                if(p1.val <= p2.val) {
-                    p1 = current;
+                prev = small;
+                if(small == p1) {
+                	p1 = p1.next;
                 } else {
-                    p2 = current;   
+                	p2 = p2.next;
                 }
             }
 
