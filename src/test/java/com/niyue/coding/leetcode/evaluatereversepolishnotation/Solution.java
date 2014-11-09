15d236421b0bcd48a5a9f877d53d98aeef65f743
Ni Yue <niyue.com@gmail.com>
2014-11-09 14:04:15 +0800

    Solve the reverse polish notation evaluator problem.

---
+++ src/test/java/com/niyue/coding/leetcode/evaluatereversepolishnotation/Solution.java
@@ -0,0 +1,42 @@
+package com.niyue.coding.leetcode.evaluatereversepolishnotation;
+
+import java.util.Arrays;
+import java.util.Set;
+
+public class Solution {
+    private final Set<String> opSet = new java.util.HashSet<String>(Arrays.asList("+", "-", "*", "/"));
+    
+    java.util.Deque<Integer> stack = new java.util.ArrayDeque<Integer>();
+    public int evalRPN(String[] tokens) {
+        int value = 0;
+        for(int i = 0; i < tokens.length; i++) {
+            if(isOperator(tokens[i])) {
+                int b = stack.removeFirst();
+                int a = stack.removeFirst();
+                value = calculate(a, b, tokens[i]);
+                stack.addFirst(value);
+            } else {
+                stack.addFirst(Integer.parseInt(tokens[i]));
+            }
+        }    
+        return stack.peekFirst();
+    }
+    
+    private boolean isOperator(String s) {
+        return opSet.contains(s);
+    }
+    
+    private int calculate(int a, int b, String op) {
+        int val = 0;
+        if(op.equals("+")) {
+            val = a + b;
+        } else if(op.equals("-")) {
+            val = a - b;
+        } else if(op.equals("*")) {
+            val = a * b;
+        } else {
+            val = a / b;
+        }
+        return val;
+    }
+}
